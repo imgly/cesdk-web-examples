99447c4bb95bd7a654d117bb911f8bebba0007bc
Malte Baumann <malte.baumann@img.ly>
2021-10-29 16:18:13 +0200

    v1.1.1

--- configure-callbacks/index.js
+++ configure-callbacks/index.js
@@ -1,7 +1,7 @@
-import 'https://cdn.img.ly/packages/imgly/cesdk-js/1.1.0/cesdk.umd.js';
+import 'https://cdn.img.ly/packages/imgly/cesdk-js/1.1.1/cesdk.umd.js';
 
 let config = {
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.1.0/assets',
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.1.1/assets',
   // highlight-callbacks
   callbacks: {
     // highlight-onlog
