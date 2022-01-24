dfb45cc8bd5043af8658797d3ac63cfa92f3d712
Malte Baumann <malte.baumann@img.ly>
2022-01-24 10:08:29 +0100

    v1.4.0-alpha.2

---
+++ configure-variables/index.js
@@ -0,0 +1,28 @@
+
+import 'https://cdn.img.ly/packages/imgly/cesdk-js/1.4.0-alpha.2/cesdk.umd.js';
+
+let config = {
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.4.0-alpha.2/assets',
+  // highlight-variables
+  variables: {
+    my_custom_variable: {
+      // highlight-value
+      value: "IMG.LY",
+      // highlight-value
+    }
+  },
+  // highlight-variables
+  // highlight-localization
+  i18n: {
+    en: {
+      variables: {
+        my_custom_variable: { label: "My Variable" }
+      }
+    }
+  }    
+  // highlight-localization
+};
+
+CreativeEditorSDK.init('#cesdk_container', config).then((instance) => {
+  /** do something with the instance of CreativeEditor SDK **/
+});
