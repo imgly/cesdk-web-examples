023ce64ecb5c89aebcb992514b97311e960cbf43
Malte Baumann <malte.baumann@img.ly>
2022-02-22 17:22:50 +0100

    v1.4.1

--- guides-headless-setup/index.js
+++ guides-headless-setup/index.js
@@ -1,9 +1,9 @@
 // highlight-import
-import 'https://cdn.img.ly/packages/imgly/cesdk-js/1.4.0/cesdk-engine.umd.js';
+import 'https://cdn.img.ly/packages/imgly/cesdk-js/1.4.1/cesdk-engine.umd.js';
 
 // highlight-setup
 const config = {
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.4.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.4.1/assets'
 };
 
 CreativeEngine.init(config, document.getElementById('cesdk_canvas')).then(
