f74ed77c98c2f531c5ec2919dcd5257bf7b6a467
Malte Baumann <malte.baumann@img.ly>
2022-02-22 17:22:50 +0100

    v1.4.1

--- guides-headless-property-api/index.js
+++ guides-headless-property-api/index.js
@@ -1,8 +1,8 @@
 // highlight-setup
-import 'https://cdn.img.ly/packages/imgly/cesdk-js/1.4.0/cesdk-engine.umd.js';
+import 'https://cdn.img.ly/packages/imgly/cesdk-js/1.4.1/cesdk-engine.umd.js';
 
 const config = {
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.4.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.4.1/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
@@ -19,115 +19,115 @@ CreativeEngine.init(config).then(async (engine) => {
 
   // Reflection
 
-  // highlight-findAll
-  const propertyNames = engine.property.findAll(star);
+  // highlight-findAllProperties
+  const propertyNames = engine.block.findAllProperties(star);
 
-  // highlight-getType
-  const pointsType = engine.property.getType('shapes/star/points'); // "Int"
+  // highlight-getPropertyType
+  const pointsType = engine.block.getPropertyType('shapes/star/points'); // "Int"
 
   // highlight-getEnumValues
-  engine.property.getEnumValues('text/horizontalAlignment');
+  engine.block.getEnumValues('text/horizontalAlignment');
 
   // Generic Properties
 
   // highlight-setBool
-  engine.property.setBool(scene, 'scene/aspectRatioLock', false);
+  engine.block.setBool(scene, 'scene/aspectRatioLock', false);
   // highlight-getBool
-  engine.property.getBool(scene, 'scene/aspectRatioLock');
+  engine.block.getBool(scene, 'scene/aspectRatioLock');
   // highlight-getInt
-  const points = engine.property.getInt(star, 'shapes/star/points');
+  const points = engine.block.getInt(star, 'shapes/star/points');
   // highlight-setInt
-  engine.property.setInt(star, 'shapes/star/points', points + 2);
+  engine.block.setInt(star, 'shapes/star/points', points + 2);
   // highlight-setFloat
-  engine.property.setFloat(star, 'shapes/star/innerDiameter', 0.75);
+  engine.block.setFloat(star, 'shapes/star/innerDiameter', 0.75);
   // highlight-getFloat
-  engine.property.getFloat(star, 'shapes/star/innerDiameter');
+  engine.block.getFloat(star, 'shapes/star/innerDiameter');
   // highlight-setString
-  engine.property.setString(text, 'text/text', '*o*');
+  engine.block.setString(text, 'text/text', '*o*');
   // highlight-getString
-  engine.property.getString(text, 'text/text');
+  engine.block.getString(text, 'text/text');
   // highlight-setColorRGBA
-  engine.property.setColorRGBA(camera, 'camera/clearColor', 1, 1, 1, 1); // White
+  engine.block.setColorRGBA(camera, 'camera/clearColor', 1, 1, 1, 1); // White
   // highlight-getColorRGBA
-  engine.property.getColorRGBA(camera, 'camera/clearColor');
+  engine.block.getColorRGBA(camera, 'camera/clearColor');
   // highlight-setEnum
-  engine.property.setEnum(text, 'text/horizontalAlignment', 'Center');
-  engine.property.setEnum(text, 'text/verticalAlignment', 'Center');
+  engine.block.setEnum(text, 'text/horizontalAlignment', 'Center');
+  engine.block.setEnum(text, 'text/verticalAlignment', 'Center');
   // highlight-setEnum
   // highlight-getEnum
-  engine.property.getEnum(text, 'text/horizontalAlignment');
+  engine.block.getEnum(text, 'text/horizontalAlignment');
 
   // Common Properties
 
   // highlight-hasCrop
-  engine.property.hasCrop(image);
+  engine.block.hasCrop(image);
   // highlight-setCropScaleX
-  engine.property.setCropScaleX(image, 2.0);
+  engine.block.setCropScaleX(image, 2.0);
   // highlight-setCropScaleY
-  engine.property.setCropScaleY(image, 1.5);
+  engine.block.setCropScaleY(image, 1.5);
   // highlight-setCropRotation
-  engine.property.setCropRotation(image, Math.PI);
+  engine.block.setCropRotation(image, Math.PI);
   // highlight-setCropTranslationX
-  engine.property.setCropTranslationX(image, -1.0);
+  engine.block.setCropTranslationX(image, -1.0);
   // highlight-setCropTranslationY
-  engine.property.setCropTranslationY(image, 1.0);
+  engine.block.setCropTranslationY(image, 1.0);
   // highlight-resetCrop
-  engine.property.resetCrop(image);
+  engine.block.resetCrop(image);
   // highlight-getCropScaleX
-  engine.property.getCropScaleX(image);
+  engine.block.getCropScaleX(image);
   // highlight-getCropScaleY
-  engine.property.getCropScaleY(image);
+  engine.block.getCropScaleY(image);
   // highlight-getCropRotation
-  engine.property.getCropRotation(image);
+  engine.block.getCropRotation(image);
   // highlight-getCropTranslationX
-  engine.property.getCropTranslationX(image);
+  engine.block.getCropTranslationX(image);
   // highlight-getCropTranslationY
-  engine.property.getCropTranslationY(image);
+  engine.block.getCropTranslationY(image);
 
   // highlight-hasOpacity
-  engine.property.hasOpacity(image);
+  engine.block.hasOpacity(image);
   // highlight-setOpacity
-  engine.property.setOpacity(image, 0.5);
+  engine.block.setOpacity(image, 0.5);
   // highlight-getOpacity
-  engine.property.getOpacity(image, 0.5);
+  engine.block.getOpacity(image, 0.5);
 
   // highlight-hasFillColor
-  if (engine.property.hasFillColor(star)) {
+  if (engine.block.hasFillColor(star)) {
     // highlight-setFillColorRGBA
-    engine.property.setFillColorRGBA(star, 1, 1, 0, 1); // Yellow
+    engine.block.setFillColorRGBA(star, 1, 1, 0, 1); // Yellow
     // highlight-getFillColorRGBA
-    engine.property.getFillColorRGBA(star);
+    engine.block.getFillColorRGBA(star);
     // highlight-setFillColorEnabled
-    engine.property.setFillColorEnabled(star, true);
+    engine.block.setFillColorEnabled(star, true);
     // highlight-isFillColorEnabled
-    engine.property.isFillColorEnabled(star);
+    engine.block.isFillColorEnabled(star);
   }
 
   // highlight-hasBackgroundColor
-  if (engine.property.hasBackgroundColor(page)) {
+  if (engine.block.hasBackgroundColor(page)) {
     // highlight-setBackgroundColorRGBA
-    engine.property.setBackgroundColorRGBA(page, 1, 0, 0, 1); // Red
+    engine.block.setBackgroundColorRGBA(page, 1, 0, 0, 1); // Red
     // highlight-getBackgroundColorRGBA
-    engine.property.getBackgroundColorRGBA(page);
+    engine.block.getBackgroundColorRGBA(page);
     // highlight-setBackgroundColorEnabled
-    engine.property.setBackgroundColorEnabled(page, true);
+    engine.block.setBackgroundColorEnabled(page, true);
     // highlight-isBackgroundColorEnabled
-    engine.property.isBackgroundColorEnabled(page);
+    engine.block.isBackgroundColorEnabled(page);
   }
 
   // highlight-hasOutline
-  if (engine.property.hasOutline(star)) {
+  if (engine.block.hasOutline(star)) {
     // highlight-setOutlineColorRGBA
-    engine.property.setOutlineColorRGBA(star, 1, 0.5, 0, 1); // Orange
+    engine.block.setOutlineColorRGBA(star, 1, 0.5, 0, 1); // Orange
     // highlight-getOutlineColorRGBA
-    engine.property.getOutlineColorRGBA(star);
+    engine.block.getOutlineColorRGBA(star);
     // highlight-setOutlineWidth
-    engine.property.setOutlineWidth(star, 10.0);
+    engine.block.setOutlineWidth(star, 10.0);
     // highlight-getOutlineWidth
-    engine.property.getOutlineWidth(star);
+    engine.block.getOutlineWidth(star);
     // highlight-setOutlineEnabled
-    engine.property.setOutlineEnabled(star, true);
+    engine.block.setOutlineEnabled(star, true);
     // highlight-isOutlineEnabled
-    engine.property.isOutlineEnabled(star);
+    engine.block.isOutlineEnabled(star);
   }
 });
--- integrate-with-next-js/components/CreativeEditorSDK.jsx
+++ integrate-with-next-js/components/CreativeEditorSDK.jsx
@@ -15,7 +15,7 @@ const Component = (props = {}) => {
     if (cesdk_container.current) {
       // Serve assets from IMG.LY CDN or locally
       props.config.baseURL =
-        'https://cdn.img.ly/packages/imgly/cesdk-js/1.4.0/assets';
+        'https://cdn.img.ly/packages/imgly/cesdk-js/1.4.1/assets';
 
       CreativeEditorSDK.init(cesdk_container.current, props.config).then(
         (instance) => {
