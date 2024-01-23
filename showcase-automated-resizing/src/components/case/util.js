0509f5b1aa691aa526e31593e49a825fcdec7295
Mirko Budszuhn <mirko314@users.noreply.github.com>
2024-01-23 11:24:43 +0000

    Remove files

--- showcase-automated-resizing/src/components/case/util.js
+++
@@ -1,2 +0,0 @@
-export const caseAssetPath = (path, caseId = 'automated-resizing') =>
-  `${window.location.protocol + "//" + window.location.host}/cases/${caseId}${path}`;
--- showcase-photobook-ui/src/components/case/util.js
+++
@@ -1,2 +0,0 @@
-export const caseAssetPath = (path, caseId = 'photobook-ui') =>
-  `${window.location.protocol + "//" + window.location.host}/cases/${caseId}${path}`;
