924d89024b7814811c3c9f260f727557a82c8ee6
Mirko Budszuhn <mirko314@users.noreply.github.com>
2024-01-23 11:24:43 +0000

    Remove files

--- showcase-apparel-ui/src/components/case/util.js
+++
@@ -1,2 +0,0 @@
-export const caseAssetPath = (path, caseId = 'apparel-ui') =>
-  `${window.location.protocol + "//" + window.location.host}/cases/${caseId}${path}`;
--- showcase-headless-design/src/components/case/util.js
+++
@@ -1,2 +0,0 @@
-export const caseAssetPath = (path, caseId = 'headless-design') =>
-  `${window.location.protocol + "//" + window.location.host}/cases/${caseId}${path}`;
