8a274c6ffaae1ab9a5efe2c6bb3c8bf82091d0f0
Intergalactic Igor <malte.baumann+bot@img.ly>
2024-03-04 13:56:43 +0100

    Update examples for v1.22.0-rc.1

--- engine-guides-event-api/index.js
+++ engine-guides-event-api/index.js
@@ -1,24 +1,30 @@
 // highlight-setup
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.17.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.22.0-rc.1/index.js';
 
 const config = {
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.17.0/assets'
+  license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
+  userId: 'guides-user',
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.22.0-rc.1/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
+  document.getElementById('cesdk_container').append(engine.element);
+
   const scene = engine.scene.create();
   const page = engine.block.create('page');
   engine.block.appendChild(scene, page);
   // highlight-setup
 
-  const star = engine.block.create('shapes/star');
-  engine.block.appendChild(page, star);
+  const block = engine.block.create('graphic');
+  engine.block.setShape(block, engine.block.createShape('star'));
+  engine.block.setFill(block, engine.block.createFill('color'));
+  engine.block.appendChild(page, block);
 
   // highlight-subscribe
-  let unsubscribe = engine.event.subscribe([star], (events) => {
+  let unsubscribe = engine.event.subscribe([block], (events) => {
     for (var i = 0; i < events.length; i++) {
       let event = events[i];
-  // highlight-subscribe
+      // highlight-subscribe
       console.log('Event:', event.type, event.block);
       if (engine.block.isValid(event.block)) {
         console.log('Block type:', engine.block.getType(event.block));
@@ -27,9 +33,9 @@ CreativeEngine.init(config).then(async (engine) => {
   });
 
   await sleep(1000);
-  engine.block.setRotation(star, 0.5 * Math.PI);
+  engine.block.setRotation(block, 0.5 * Math.PI);
   await sleep(1000);
-  engine.block.destroy(star);
+  engine.block.destroy(block);
   await sleep(1000);
 
   // highlight-unsubscribe
