2801d6049c2c1f16e61beadec235dfc290283959
Intergalactic Igor <malte.baumann+bot@img.ly>
2024-03-22 16:14:24 +0100

    Update examples for v1.23.0-rc.2

--- engine-guides-create-scene-from-image-element/index.js
+++ engine-guides-create-scene-from-image-element/index.js
@@ -1,12 +1,14 @@
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.17.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.23.0-rc.2/index.js';
 
 // highlight-element
-const element = document.getElementById("image-element");
+const element = document.getElementById('image-element');
 const imageURL = element.src;
 // highlight-element
 
 const config = {
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.17.0/assets'
+  license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
+  userId: 'guides-user',
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.23.0-rc.2/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
@@ -14,14 +16,14 @@ CreativeEngine.init(config).then(async (engine) => {
   let scene = await engine.scene.createFromImage(imageURL);
   // highlight-initialImageURL
 
-  // highlight-find-image
-  // Find the automatically added image element in the scene.
-  const image = engine.block.findByType('image')[0];
-  // highlight-find-image
+  // highlight-find-block
+  // Find the automatically added graphic block with an image fill in the scene.
+  const block = engine.block.findByType('graphic')[0];
+  // highlight-find-block
 
   // highlight-set-opacity
   // Change its opacity.
-  engine.block.setOpacity(image, 0.5);
+  engine.block.setOpacity(block, 0.5);
   // highlight-set-opacity
 
   // Attach engine canvas to DOM
