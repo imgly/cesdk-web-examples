e531c9e07728c4264d7d6f14fb87d46e2ba7f762
Intergalactic Igor <malte.baumann+bot@img.ly>
2024-06-20 13:19:32 +0200

    Update examples for v1.29.0

---
+++ guides-fonts-typefaces/index.js
@@ -0,0 +1,61 @@
+import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.29.0/index.js';
+
+const config = {
+  license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
+  userId: 'guides-user',
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.29.0/assets',
+  theme: 'light',
+  ui: {
+    typefaceLibraries: [
+      // highlight-remove-default-typefaces
+      // 'ly.img.typeface',
+      'my-custom-typefaces'
+    ]
+  },
+  // highlight-remove-default-typefaces
+  presets: {
+    callbacks: { onUpload: 'local' } // Enable local uploads in Asset Library.
+  }
+};
+
+CreativeEditorSDK.create('#cesdk_container', config).then(async (instance) => {
+  // Do something with the instance of CreativeEditor SDK, for example:
+  // Populate the asset library with default / demo asset sources.
+  instance.addDefaultAssetSources();
+  instance.addDemoAssetSources({ sceneMode: 'Design' });
+
+  // highlight-typefaces
+  // Add a custom typeface asset source.
+  instance.engine.asset.addLocalSource('my-custom-typefaces');
+  instance.engine.asset.addAssetToSource('my-custom-typefaces', {
+    id: 'orbitron',
+    label: {
+      en: 'Orbitron'
+    },
+    payload: {
+      typeface: {
+        // highlight-family
+        name: 'Orbitron',
+        // highlight-fonts
+        fonts: [
+          {
+            uri: `${window.location.protocol}//${window.location.host}/Orbitron-Regular.ttf`,
+            subFamily: 'Regular',
+            weight: 'regular',
+            style: 'normal'
+          },
+          {
+            uri: `${window.location.protocol}//${window.location.host}/Orbitron-Bold.ttf`,
+            subFamily: 'Bold',
+            weight: 'bold',
+            style: 'normal'
+          }
+        ]
+        // highlight-fonts
+      }
+    }
+  });
+  // highlight-typefaces
+
+  await instance.createDesignScene();
+});
