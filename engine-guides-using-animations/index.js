461d8a491d11ebd68c2c6798550ea0bdf8d0423c
Intergalactic Igor <malte.baumann+bot@img.ly>
2024-07-05 22:17:38 +0200

    Update examples for v1.31.0-rc.0

---
+++ engine-guides-using-animations/index.js
@@ -0,0 +1,88 @@
+// highlight-setup
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0-rc.0/index.js';
+
+const config = {
+  license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
+  userId: 'guides-user',
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0-rc.0/assets'
+};
+
+CreativeEngine.init(config).then(async (engine) => {
+  document.getElementById('cesdk_container').append(engine.element);
+
+  const scene = engine.scene.createVideo();
+  const page = engine.block.create('page');
+  engine.block.setWidth(page, 1920);
+  engine.block.setHeight(page, 1080);
+  engine.block.appendChild(scene, page);
+
+  engine.editor.setSettingColor('clearColor', { r: 0.2, g: 0.2, b: 0.2, a: 1 });
+
+  const graphic = engine.block.create('graphic');
+  const imageFill = engine.block.createFill('image');
+  engine.block.setString(
+    imageFill,
+    'fill/image/imageFileURI',
+    'https://cdn.img.ly/assets/v3/ly.img.sticker/images/emoticons/imgly_sticker_emoticons_star.svg'
+  );
+  engine.block.setFill(graphic, imageFill);
+  engine.block.setShape(graphic, engine.block.createShape('rect'));
+  engine.block.setWidth(graphic, 500);
+  engine.block.setHeight(graphic, 500);
+  engine.block.setPositionX(graphic, (1920 - 500) / 2);
+  engine.block.setPositionY(graphic, (1080 - 500) / 2);
+  engine.block.appendChild(page, graphic);
+
+  engine.scene.zoomToBlock(page, 60, 60, 60, 60);
+  // highlight-setup
+
+  // highlight-supportsAnimation
+  if (!engine.block.supportsAnimation(graphic)) {
+    return;
+  }
+  // highlight-supportsAnimation
+
+  // highlight-createAnimation
+  const slideInAnimation = engine.block.createAnimation('slide');
+  const breathingLoopAnimation = engine.block.createAnimation('breathing_loop');
+  const fadeOutAnimation = engine.block.createAnimation('fade');
+  // highlight-createAnimation
+  // highlight-setInAnimation
+  engine.block.setInAnimation(graphic, slideInAnimation);
+  // highlight-setInAnimation
+  // highlight-setLoopAnimation
+  engine.block.setLoopAnimation(graphic, breathingLoopAnimation);
+  // highlight-setLoopAnimation
+  // highlight-setOutAnimation
+  engine.block.setOutAnimation(graphic, fadeOutAnimation);
+  // highlight-setOutAnimation
+  // highlight-getAnimation
+  const animation = engine.block.getLoopAnimation(graphic);
+  const animationType = engine.block.getType(animation);
+  // highlight-getAnimation
+
+  // highlight-replaceAnimation
+  const squeezeLoopAnimation = engine.block.createAnimation('squeeze_loop');
+  engine.block.destroy(engine.block.getLoopAnimation(graphic));
+  engine.block.setLoopAnimation(graphic, squeezeLoopAnimation);
+  /* The following line would also destroy all currently attached animations */
+  // engine.block.destroy(graphic);
+  // highlight-replaceAnimation
+
+  // highlight-getProperties
+  const allAnimationProperties = engine.block.findAllProperties(slideInAnimation);
+  // highlight-getProperties
+  // highlight-modifyProperties
+  engine.block.setFloat(slideInAnimation, 'animation/slide/direction', 0.5 * Math.PI);
+  // highlight-modifyProperties
+  // highlight-changeDuration
+  engine.block.setDuration(slideInAnimation, 0.6);
+  // highlight-changeDuration
+  // highlight-changeEasing
+  engine.block.setEnum(slideInAnimation, 'animationEasing', 'EaseOut');
+  console.log("Available easing options:", engine.block.getEnumValues('animationEasing'));
+  // highlight-changeEasing
+
+  engine.block.setPlaying(page, true);
+  engine.block.setLooping(page, true);
+});
--- showcase-placeholders/src/components/case/CaseComponent.jsx
+++ showcase-placeholders/src/components/case/CaseComponent.jsx
@@ -128,30 +128,11 @@ const CaseComponent = () => {
           size="md"
         />
       </div>
-      <div style={cesdkWrapperStyle} key={currentRole + currentScene}>
-        <div ref={cesdkContainer} style={cesdkStyle}></div>
+      <div className="cesdkWrapperStyle" key={currentRole + currentScene}>
+        <div ref={cesdkContainer} className="cesdkStyle"></div>
       </div>
     </div>
   );
-};
-
-const cesdkStyle = {
-  position: 'absolute',
-  top: 0,
-  right: 0,
-  bottom: 0,
-  left: 0
-};
-
-const cesdkWrapperStyle = {
-  position: 'relative',
-  minHeight: '640px',
-  overflow: 'hidden',
-  flexGrow: 1,
-  display: 'flex',
-  borderRadius: '0.75rem',
-  boxShadow:
-    '0px 0px 2px rgba(22, 22, 23, 0.25), 0px 4px 6px -2px rgba(22, 22, 23, 0.12), 0px 2px 2.5px -2px rgba(22, 22, 23, 0.12), 0px 1px 1.75px -2px rgba(22, 22, 23, 0.12)'
 };
 
 export default CaseComponent;
