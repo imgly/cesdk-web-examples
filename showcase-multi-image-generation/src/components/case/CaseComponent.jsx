d17948081e7326d6b8ac381e76d48170d9d584d4
Mirko Budszuhn <mirko314@users.noreply.github.com>
2024-09-23 17:00:14 +0200

    Update 1.35.0

--- showcase-multi-image-generation/src/components/case/CaseComponent.jsx
+++ showcase-multi-image-generation/src/components/case/CaseComponent.jsx
@@ -3,23 +3,45 @@
 import CreativeEngine from '@cesdk/engine';
 import React, { useEffect, useRef, useState } from 'react';
 import LoadingSpinner from '@/components/ui/LoadingSpinner/LoadingSpinner';
+import { hexToRgba } from './lib/ColorUtilities';
+import classes from './CaseComponent.module.css';
 
-const ID_FROM_RESTAURANT_REGEX = /yelp\.de\/biz\/([^?]*).*?$/;
 const caseAssetPath = (path, caseId = 'multi-image-generation') =>
   `${process.env.NEXT_PUBLIC_URL_HOSTNAME}${process.env.NEXT_PUBLIC_URL}/cases/${caseId}${path}`;
 
-const YELP_EXAMPLES = [
+const EXAMPLES = [
   {
-    label: 'Ice Cream',
-    value: 'https://www.yelp.de/biz/i-am-love-bochum-2'
+    name: 'Bean there Bean good',
+    photo_path: '/images/photo-bean.png',
+    price: '$$',
+    review_count: 281,
+    rating: 1,
+    card_path: '/images/card-bean.png',
+    logo_path: '/images/logo-bean.png',
+    primary_color: '#050087',
+    secondary_color: '#F1E1C7'
   },
   {
-    label: 'Coffee',
-    value: 'https://www.yelp.de/biz/r%C3%B6st-art-bochum'
+    name: 'Scoop\nthere it is',
+    photo_path: '/images/photo-scoop.png',
+    price: '$',
+    review_count: 114,
+    rating: 5,
+    card_path: '/images/card-scoop.png',
+    logo_path: '/images/logo-scoop.png',
+    primary_color: '#EB11D5',
+    secondary_color: '#85EAD1'
   },
   {
-    label: 'Burger',
-    value: 'https://www.yelp.de/biz/burgerado-bochum'
+    name: 'BUN intended',
+    photo_path: '/images/photo-bun.png',
+    price: '$$$',
+    review_count: 65,
+    rating: 3,
+    card_path: '/images/card-bun.png',
+    logo_path: '/images/logo-bun.png',
+    primary_color: '#2E573E',
+    secondary_color: '#E4A341'
   }
 ];
 
@@ -54,37 +76,81 @@ const replaceImage = (cesdk, imageName, newUrl) => {
   cesdk.block.resetCrop(img);
 };
 
-const fillTemplate = (cesdk, yelpData) => {
-  if (!yelpData) {
+const fillTemplate = (cesdk, restaurantData) => {
+  if (!restaurantData) {
     return false;
   }
-  replaceImage(cesdk, 'RestaurantPhoto', yelpData.image_url);
-  cesdk.variable.setString('Name', yelpData.name || '');
-  cesdk.variable.setString('$$', yelpData.price || '');
-  cesdk.variable.setString('Count', yelpData.review_count.toString() || '');
-  const rating = yelpData.rating;
-  const ratingRoundedToHalf = Math.round(rating * 2) / 2;
-  const ratingImageUrl = caseAssetPath(
-    `/images/${ratingRoundedToHalf.toString().replace('.', '_')}.png`
+  replaceImage(cesdk, 'RestaurantPhoto', caseAssetPath(restaurantData.photo_path));
+  const photoBlock = cesdk.block.findByName('RestaurantPhoto')[0];
+  cesdk.block.setContentFillMode(photoBlock, 'Cover');
+  replaceImage(cesdk, 'RestaurantLogo', caseAssetPath(restaurantData.logo_path));
+  cesdk.variable.setString('Name', restaurantData.name || '');
+  cesdk.variable.setString('$$', restaurantData.price || '');
+  cesdk.variable.setString('Count', restaurantData.review_count.toString() || '');
+  // Apply colors based on black/white-ness of the previous color
+  const primaryColor = hexToRgba(restaurantData.primary_color);
+  const secondaryColor = hexToRgba(restaurantData.secondary_color);
+  cesdk.block.findAll().forEach((block) => {
+    // handle text blocks differently
+    if (cesdk.block.getType(block) === '//ly.img.ubq/text') {
+      const preColor = cesdk.block.getTextColors(block)[0];
+      if (preColor.r === 1 && preColor.g === 1 && preColor.b === 1) {
+        //white
+        cesdk.block.setTextColor(block, secondaryColor);
+      } else if (preColor.r === 0 && preColor.g === 0 && preColor.b === 0) {
+        //black
+        cesdk.block.setTextColor(block, primaryColor);
+      }
+    } else {
+      if (cesdk.block.supportsFill(block) && cesdk.block.hasFill(block)) {
+        const fillBlock = cesdk.block.getFill(block);
+        if (cesdk.block.getType(fillBlock) === '//ly.img.ubq/fill/color') {
+          const preColor = cesdk.block.getColor(
+            fillBlock,
+            'fill/color/value'
+          );
+          if (preColor.r === 1 && preColor.g === 1 && preColor.b === 1) {
+            //white
+            cesdk.block.setColor(
+              fillBlock,
+              'fill/color/value',
+              secondaryColor
+            );
+          } else if (
+            preColor.r === 0 &&
+            preColor.g === 0 &&
+            preColor.b === 0
+          ) {
+            //black
+            cesdk.block.setColor(fillBlock, 'fill/color/value', primaryColor);
+          }
+        }
+      }
+    }
+  });
+  // Fill rating stars' color
+  const onColor = cesdk.block.getColor(
+    cesdk.block.findByName('Rating1')[0],
+    'fill/solid/color'
   );
-  replaceImage(cesdk, 'ReviewStars', ratingImageUrl);
-};
-
-const yelpIdFromUrl = (url) => {
-  const match = url.match(ID_FROM_RESTAURANT_REGEX);
-  if (match && match[1]) {
-    return match[1];
+  const offColor = cesdk.block.getColor(
+    cesdk.block.findByName('Rating5')[0],
+    'fill/solid/color'
+  );
+  for (let i = 1; i < 6; i++) {
+    cesdk.block.setColor(
+      cesdk.block.findByName(`Rating${i}`)[0],
+      'fill/solid/color',
+      restaurantData.rating >= i ? onColor : offColor
+    );
   }
-  return null;
 };
 
 const CaseComponent = () => {
   const engineRef = useRef(null);
 
   const [isLoading, setIsLoading] = useState(false);
-  const [yelpData, setYelpData] = useState(null);
-  const [yelpId, setYelpId] = useState(null);
-  const [yelpUrl, setYelpUrl] = useState('');
+  const [restaurantName, setRestaurantName] = useState(null);
   const [reviewBlobs, setReviewBlobs] = useState(new Array(3).fill(null));
 
   useEffect(() => {
@@ -100,101 +166,61 @@ const CaseComponent = () => {
     return function shutdownCreativeEngine() {
       engineRef?.current?.dispose();
     };
-    // eslint-disable-next-line
   }, []);
 
-  useEffect(() => {
-    if (yelpId) {
-      fetchApiData(yelpId);
-    }
-  }, [yelpId]);
-
-  useEffect(() => {
+  const selectRestaurant = async (restaurantData) => {
     const engine = engineRef?.current;
-    if (!engine || !yelpData) {
+    if (!engine || !restaurantData) {
       return;
     }
-    async function renderTemplates() {
-      // This can not be done in parallel.
-      for (const [index, sceneUrl] of TEMPLATE_PATHS.entries()) {
-        await engine.scene.loadFromURL(caseAssetPath(sceneUrl.scenePath));
-        fillTemplate(engineRef.current, yelpData);
-        const blob = await engine.block.export(
-          engine.scene.get(),
-          'image/jpeg'
-        );
-        setReviewBlobs((oldBlobs) => {
-          oldBlobs[index] = {
-            isLoading: false,
-            src: URL.createObjectURL(blob)
-          };
-          return [...oldBlobs];
-        });
-        await new Promise((resolve) => setTimeout(resolve, 400));
-      }
-    }
-    renderTemplates();
-  }, [yelpData]);
-
-  const fetchApiData = async (id) => {
-    setIsLoading(true);
     setReviewBlobs((oldBlobs) => [
       ...oldBlobs.map((blob) => ({ ...blob, isLoading: true }))
     ]);
-    const res = await fetch(
-      `https://europe-west3-img-ly.cloudfunctions.net/demos-yelp-api-proxy2?id=` +
-        id
-    );
-    const resJson = await res.json();
-    if (resJson.id) {
-      setYelpData(resJson);
+    // This can not be done in parallel.
+    for (const [index, sceneUrl] of TEMPLATE_PATHS.entries()) {
+      await engine.scene.loadFromURL(caseAssetPath(sceneUrl.scenePath));
+      fillTemplate(engineRef.current, restaurantData);
+      const blob = await engine.block.export(engine.scene.get(), 'image/jpeg');
+      setReviewBlobs((oldBlobs) => {
+        oldBlobs[index] = {
+          isLoading: false,
+          src: URL.createObjectURL(blob)
+        };
+        return [...oldBlobs];
+      });
     }
     setIsLoading(false);
   };
 
   return (
-    <div className="gap-lg flex flex-grow flex-col items-center justify-center">
+    <div
+      className="flex flex-grow flex-col items-center justify-center"
+      style={{ gap: '64px' }}
+    >
       <div className="gap-sm flex flex-col items-center">
-        <h3 className="h4">Paste Yelp Restaurant URL</h3>
-        <div className="gap-sm flex flex-wrap">
-          <input
-            type="text"
-            value={yelpUrl}
-            placeholder={`e.g. ${YELP_EXAMPLES[0].value}`}
-            onChange={(e) => setYelpUrl(e.target.value)}
-            style={inputStyle}
-          />
-          {isLoading ? (
-            <button disabled className="button button--primary">
-              Loading
-            </button>
-          ) : (
-            <button
-              onClick={() => {
-                if (!yelpUrl) {
-                  setYelpUrl(YELP_EXAMPLES[0].value);
-                }
-                setYelpId(yelpIdFromUrl(yelpUrl || YELP_EXAMPLES[0].value));
-              }}
-              className="button button--primary"
-            >
-              Generate
-            </button>
-          )}
-        </div>
+        <h3 className="h4">Select Restaurant</h3>
         <div className="paragraphSmall" style={paragraphStyle}>
-          <span>Or try these examples:</span>
-          <div className="gap-xs flex">
-            {YELP_EXAMPLES.map(({ label, value }) => (
+          <div className="gap-sm flex">
+            {EXAMPLES.map((example) => (
               <button
-                key={value}
-                style={exampleButtonStyle}
+                key={example.name}
+                className={`${classes.example_button} ${
+                  restaurantName === example.name ? classes.selected : ''
+                }`}
+                disabled={restaurantName !== example.name && isLoading}
+                style={{ backgroundColor: example.secondary_color }}
                 onClick={() => {
-                  setYelpUrl(value);
-                  setYelpId(yelpIdFromUrl(value));
+                  setIsLoading(true);
+                  setRestaurantName(example.name);
+                  selectRestaurant(example);
                 }}
               >
-                {label}
+                <img
+                  className="object-contain w-auto"
+                  src={caseAssetPath(example.card_path)}
+                  alt={example.name}
+                />
+                <div className={classes.overlay}></div>
               </button>
             ))}
           </div>
@@ -229,10 +255,6 @@ const CaseComponent = () => {
   );
 };
 
-const inputStyle = {
-  minWidth: 320
-};
-
 const imageWrapper = {
   display: 'flex',
   alignItems: 'center',
@@ -246,7 +268,5 @@ const paragraphStyle = {
   columnGap: '1rem',
   flexWrap: 'wrap'
 };
-
-const exampleButtonStyle = { color: '#471AFF' };
 
 export default CaseComponent;
