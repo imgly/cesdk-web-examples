0ed9ef1f0bd35af8cf937527b8440f0a0d8b032f
Intergalactic Igor <malte.baumann+bot@img.ly>
2024-12-10 10:29:39 +0100

    Update examples for v1.41.0-rc.1

--- engine-guides-text-properties/index.js
+++ engine-guides-text-properties/index.js
@@ -1,9 +1,9 @@
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.41.0-rc.1/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.41.0-rc.1/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
@@ -18,47 +18,26 @@ CreativeEngine.init(config).then(async (engine) => {
   engine.block.setWidthMode(text, 'Auto');
   engine.block.setHeightMode(text, 'Auto');
 
-  // highlight-replaceText
   engine.block.replaceText(text, 'Hello World');
-  // highlight-replaceText
-  // highlight-replaceText-single-index
   // Add a "!" at the end of the text
   engine.block.replaceText(text, '!', 11);
-  // highlight-replaceText-single-index
-  // highlight-replaceText-range
   // Replace "World" with "Alex"
   engine.block.replaceText(text, 'Alex', 6, 11);
-  // highlight-replaceText-range
 
   engine.scene.zoomToBlock(text, 100, 100, 100, 100);
 
-  // highlight-removeText
   // Remove the "Hello "
   engine.block.removeText(text, 0, 6);
-  // highlight-removeText
 
-  // highlight-setTextColor
   engine.block.setTextColor(text, { r: 1.0, g: 1.0, b: 0.0, a: 1.0 });
-  // highlight-setTextColor
-  // highlight-setTextColor-range
   engine.block.setTextColor(text, { r: 0.0, g: 0.0, b: 0.0, a: 1.0 }, 1, 4);
-  // highlight-setTextColor-range
-  // highlight-getTextColors
   const allColors = engine.block.getTextColors(text);
-  // highlight-getTextColors
-  // highlight-getTextColors-range
   const colorsInRange = engine.block.getTextColors(text, 2, 5);
-  // highlight-getTextColors-range
 
-  // highlight-setTextCase
   engine.block.setTextCase(text, 'Titlecase');
-  // highlight-setTextCase
 
-  // highlight-getTextCases
   const textCases = engine.block.getTextCases(text);
-  // highlight-getTextCases
 
-  // highlight-setFont
   const typeface = {
     name: 'Roboto',
     fonts: [
@@ -85,29 +64,29 @@ CreativeEngine.init(config).then(async (engine) => {
     ]
   };
   engine.block.setFont(text, typeface.fonts[3].uri, typeface);
-  // highlight-setFont
 
-  // highlight-getTypeface
-  const currentTypeface = engine.block.getTypeface(text);
-  // highlight-getTypeface
+  engine.block.setTypeface(text, typeface);
 
-  // highlight-toggleBold
+  const currentDefaultTypeface = engine.block.getTypeface(text);
+
+  const currentTypefaces = engine.block.getTypefaces(text);
+  const currentTypefacesOfRange = engine.block.getTypefaces(text, 1, 4);
+
   if (engine.block.canToggleBoldFont(text)) {
     engine.block.toggleBoldFont(text);
   }
-  // highlight-toggleBold
+  if (engine.block.canToggleBoldFont(text, 1, 4)) {
+    engine.block.toggleBoldFont(text, 1, 4);
+  }
 
-  // highlight-toggleItalic
   if (engine.block.canToggleItalicFont(text)) {
     engine.block.toggleItalicFont(text);
   }
-  // highlight-toggleItalic
+  if (engine.block.canToggleItalicFont(text, 1, 4)) {
+    engine.block.toggleItalicFont(text, 1, 4);
+  }
 
-  // highlight-getTextFontWeights
   const fontWeights = engine.block.getTextFontWeights(text);
-  // highlight-getTextFontWeights
 
-  // highlight-getTextFontStyles
   const fontStyles = engine.block.getTextFontStyles(text);
-  // highlight-getTextFontStyles
 });
