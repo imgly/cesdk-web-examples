cce061c72835ce2aebf0c1e64c0e2a9d1ee986bf
Intergalactic Igor <malte.baumann+bot@img.ly>
2024-12-16 17:59:41 +0100

    Update examples for v1.42.0-rc.0

--- engine-guides-using-animations/index.js
+++ engine-guides-using-animations/index.js
@@ -1,10 +1,9 @@
-// highlight-setup
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.42.0-rc.0/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.33.0/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
@@ -34,54 +33,57 @@ CreativeEngine.init(config).then(async (engine) => {
   engine.block.appendChild(page, graphic);
 
   engine.scene.zoomToBlock(page, 60, 60, 60, 60);
-  // highlight-setup
 
-  // highlight-supportsAnimation
   if (!engine.block.supportsAnimation(graphic)) {
     return;
   }
-  // highlight-supportsAnimation
 
-  // highlight-createAnimation
   const slideInAnimation = engine.block.createAnimation('slide');
   const breathingLoopAnimation = engine.block.createAnimation('breathing_loop');
   const fadeOutAnimation = engine.block.createAnimation('fade');
-  // highlight-createAnimation
-  // highlight-setInAnimation
   engine.block.setInAnimation(graphic, slideInAnimation);
-  // highlight-setInAnimation
-  // highlight-setLoopAnimation
   engine.block.setLoopAnimation(graphic, breathingLoopAnimation);
-  // highlight-setLoopAnimation
-  // highlight-setOutAnimation
   engine.block.setOutAnimation(graphic, fadeOutAnimation);
-  // highlight-setOutAnimation
-  // highlight-getAnimation
   const animation = engine.block.getLoopAnimation(graphic);
   const animationType = engine.block.getType(animation);
-  // highlight-getAnimation
 
-  // highlight-replaceAnimation
   const squeezeLoopAnimation = engine.block.createAnimation('squeeze_loop');
   engine.block.destroy(engine.block.getLoopAnimation(graphic));
   engine.block.setLoopAnimation(graphic, squeezeLoopAnimation);
   /* The following line would also destroy all currently attached animations */
   // engine.block.destroy(graphic);
-  // highlight-replaceAnimation
 
-  // highlight-getProperties
   const allAnimationProperties = engine.block.findAllProperties(slideInAnimation);
-  // highlight-getProperties
-  // highlight-modifyProperties
   engine.block.setFloat(slideInAnimation, 'animation/slide/direction', 0.5 * Math.PI);
-  // highlight-modifyProperties
-  // highlight-changeDuration
   engine.block.setDuration(slideInAnimation, 0.6);
-  // highlight-changeDuration
-  // highlight-changeEasing
   engine.block.setEnum(slideInAnimation, 'animationEasing', 'EaseOut');
   console.log("Available easing options:", engine.block.getEnumValues('animationEasing'));
-  // highlight-changeEasing
+
+  const text = engine.block.create('text');
+  const textAnimation = engine.block.createAnimation('baseline');
+  engine.block.setInAnimation(text, textAnimation);
+  engine.block.appendChild(page, text);
+  engine.block.setPositionX(text, 100);
+  engine.block.setPositionY(text, 100);
+  engine.block.setWidthMode(text, 'Auto');
+  engine.block.setHeightMode(text, 'Auto');
+  engine.block.replaceText(text, "You can animate text\nline by line,\nword by word,\nor character by character\nwith CE.SDK");
+  engine.block.setEnum(textAnimation, 'textAnimationWritingStyle', 'Word');
+  engine.block.setDuration(textAnimation, 2.0);
+  engine.block.setEnum(textAnimation, 'animationEasing', 'EaseOut');
+
+  const text2 = engine.block.create('text');
+  const textAnimation2 = engine.block.createAnimation('pan');
+  engine.block.setInAnimation(text2, textAnimation2);
+  engine.block.appendChild(page, text2);
+  engine.block.setPositionX(text2, 100);
+  engine.block.setPositionY(text2, 500);
+  engine.block.setWidth(text2, 500);
+  engine.block.setHeightMode(text2, 'Auto');
+  engine.block.replaceText(text2, "You can use the textAnimationOverlap property to control the overlap between text animation segments.");
+  engine.block.setFloat(textAnimation2, 'textAnimationOverlap', 0.4);
+  engine.block.setDuration(textAnimation2, 1.0);
+  engine.block.setEnum(textAnimation2, 'animationEasing', 'EaseOut');
 
   engine.block.setPlaying(page, true);
   engine.block.setLooping(page, true);
