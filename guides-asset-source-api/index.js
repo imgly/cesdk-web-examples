f5cd9b9e2ed96a34a318bdb3a4a8ea8df0775412
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-01-13 15:35:41 +0100

    Update examples for v1.43.0-rc.0

--- guides-asset-source-api/index.js
+++ guides-asset-source-api/index.js
@@ -1,26 +1,19 @@
-import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.31.0/index.js';
+import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.43.0-rc.0/index.js';
 
 import * as unsplash from './vendor/unsplash-js.esm.js';
 
-// highlight-unsplash-api-creation
 const unsplashApi = unsplash.createApi({
   apiUrl: '...'
 });
-// highlight-unsplash-api-creation
 
-// highlight-unsplash-findAssets
 const findUnsplashAssets = async (queryData) => {
-  // highlight-unsplash-findAssets
   if (queryData.query) {
-    // highlight-unsplash-query
     const response = await unsplashApi.search.getPhotos({
       query: queryData.query,
       page: queryData.page,
       perPage: queryData.perPage
     });
-    // highlight-unsplash-query
     if (response.type === 'success') {
-      // highlight-unsplash-result-mapping
       const { results, total, total_pages } = response.response;
 
       return {
@@ -31,18 +24,12 @@ const findUnsplashAssets = async (queryData) => {
         nextPage:
           queryData.page + 1 <= total_pages ? queryData.page + 1 : undefined
       };
-      // highlight-unsplash-result-mapping
     } else if (response.type === 'error') {
-      // highlight-unsplash-error
       throw new Error(response.errors[0]);
-      // highlight-unsplash-error
     } else {
-      // highlight-unsplash-empty-fallback
       return Promise.resolve(undefined);
-      // highlight-unsplash-empty-fallback
     }
   } else {
-    // highlight-unsplash-list
     const response = await unsplashApi.photos.list({
       orderBy: 'popular',
       page: queryData.page,
@@ -68,19 +55,16 @@ const findUnsplashAssets = async (queryData) => {
     } else {
       return Promise.resolve(undefined);
     }
-    // highlight-unsplash-list
   }
 };
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.31.0/assets',
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.43.0-rc.0/assets',
   assetSources: {
-    // highlight-unsplash-definition
     unsplash: {
       findAssets: findUnsplashAssets,
-      // highlight-unsplash-credits-license
       credits: {
         name: 'Unsplash',
         url: 'https://unsplash.com/'
@@ -89,75 +73,47 @@ const config = {
         name: 'Unsplash license (free)',
         url: 'https://unsplash.com/license'
       }
-      // highlight-unsplash-credits-license
     }
-    // highlight-unsplash-definition
   }
 };
 
 const cesdk = await CreativeEditorSDK.create('#cesdk_container', config);
 await cesdk.createDesignScene();
 
-// highlight-translateToAssetResult
 function translateToAssetResult(image) {
-  // highlight-translateToAssetResult
   const artistName = image?.user?.name;
   const artistUrl = image?.user?.links?.html;
 
   return {
-    // highlight-result-id
     id: image.id,
-    // highlight-result-id
-    // highlight-result-locale
     locale: 'en',
-    // highlight-result-locale
     //
-    // highlight-result-label
     label: image.description ?? image.alt_description ?? undefined,
-    // highlight-result-label
-    // highlight-result-tags
     tags: image.tags ? image.tags.map((tag) => tag.title) : undefined,
-    // highlight-result-tags
 
     meta: {
-      // highlight-result-uri
       uri: image.urls.full,
-      // highlight-result-uri
-      // highlight-result-thumbUri
       thumbUri: image.urls.thumb,
-      // highlight-result-thumbUri
-      // highlight-result-blockType
       blockType: '//ly.img.ubq/graphic',
-      // highlight-result-blockType
-      // highlight-result-fillType
       fillType: '//ly.img.ubq/fill/image',
-      // highlight-result-fillType
-      // highlight-result-size
       width: image.width,
       height: image.height
-      // highlight-result-size
     },
 
-    // highlight-result-context
     context: {
       sourceId: 'unsplash'
     },
-    // highlight-result-context
 
-    // highlight-result-credits
     credits: artistName
       ? {
           name: artistName,
           url: artistUrl
         }
       : undefined,
-    // highlight-result-credits
 
-    // highlight-result-utm
     utm: {
       source: 'CE.SDK Demo',
       medium: 'referral'
     }
-    // highlight-result-utm
   };
 }
