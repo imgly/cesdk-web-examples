cec376caf0f6ea35eeb08942b4c8cdb6ce90fa9b
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-01-14 16:04:03 +0100

    Update examples for v1.43.0-rc.2

--- configure-basics/index.js
+++ configure-basics/index.js
@@ -1,40 +1,23 @@
-import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.31.0/index.js';
+import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.43.0-rc.2/index.js';
 
-// highlight-config
 const config = {
-  // highlight-license
   license: 'YOUR_API_KEY',
-  // highlight-license
-  // highlight-userid
   userId: 'USER_ID',
-  // highlight-userid
-  // highlight-baseurl
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.31.0/assets',
-  // highlight-baseurl
-  // highlight-locale
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.43.0-rc.2/assets',
   locale: 'en', // 'de'
-  // highlight-locale
-  // highlight-theme
   theme: 'light', // 'dark'
-  // highlight-theme
-  // highlight-role
   role: 'Creator', // 'Adopter' 'Viewer'
-  // highlight-role
   callbacks: { onUpload: 'local' }, // Enable local uploads in Asset Library.
-  // highlight-logger
   logger: (message, logLevel) => {
     console.log(`${logLevel}: ${message}}`);
   }
-  // highlight-logger
 };
-// highlight-config
 
 CreativeEditorSDK.create('#cesdk_container', config).then(async (instance) => {
   // Populate the asset library with default / demo asset sources.
   instance.addDefaultAssetSources();
   instance.addDemoAssetSources({ sceneMode: 'Design' });
 
-  // highlight-onRoleChanged
   instance.engine.editor.onRoleChanged((role) => {
     if (role === 'Adopter') {
       // Enable the filter tab in the appearance panel when previewing the
@@ -42,7 +25,6 @@ CreativeEditorSDK.create('#cesdk_container', config).then(async (instance) => {
       instance.engine.editor.setGlobalScope('appearance/filter', 'Allow');
     }
   });
-  // highlight-onRoleChanged
 
   await instance.createDesignScene();
 });
