ed8286c743094921e384b44112161b698c2f39f7
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-02-03 18:15:05 +0100

    Update examples for v1.44.0-rc.1

--- engine-guides-create-scene-from-image-blob/index.js
+++ engine-guides-create-scene-from-image-blob/index.js
@@ -1,34 +1,24 @@
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.44.0-rc.1/index.js';
 
-// highlight-blob
 const blob = await fetch('https://img.ly/static/ubq_samples/sample_4.jpg').then(
   (response) => response.blob()
 );
-// highlight-blob
-// highlight-objectURL
 const objectURL = URL.createObjectURL(blob);
-// highlight-objectURL
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.44.0-rc.1/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
-  // highlight-initialImageURL
   let scene = await engine.scene.createFromImage(objectURL);
-  // highlight-initialImageURL
 
-  // highlight-find-findByType
   // Find the automatically added graphic block in the scene that contains the image fill.
   const block = engine.block.findByType('graphic')[0];
-  // highlight-find-findByType
 
-  // highlight-set-opacity
   // Change its opacity.
   engine.block.setOpacity(block, 0.5);
-  // highlight-set-opacity
 
   // Attach engine canvas to DOM
   document.getElementById('cesdk_container').append(engine.element);
