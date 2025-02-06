713f38e63aa9f1a411f99c3909d092427bfb26c8
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-02-06 09:31:01 +0100

    Update examples for v1.44.0

--- showcase-apparel-ui/src/components/case/ui/FontSelect/FontSelect.tsx
+++ showcase-apparel-ui/src/components/case/ui/FontSelect/FontSelect.tsx
@@ -47,7 +47,7 @@ const FontSelect = ({ onSelect, activeTypeface }: FontSelectProps) => {
   const typefacesWithRef = useMemo<
     {
       typeface: Typeface;
-      ref: React.RefObject<HTMLButtonElement | null>;
+      ref: React.RefObject<HTMLButtonElement>;
       isActive: boolean;
     }[]
   >(
