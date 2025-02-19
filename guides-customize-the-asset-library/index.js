b922c66a3c862f73c29e65ca2483bc08bf9cb2f4
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-02-19 12:17:59 +0100

    Update examples for v1.45.0

--- guides-customize-the-asset-library/index.js
+++ guides-customize-the-asset-library/index.js
@@ -1,96 +1,12 @@
-import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.31.0/index.js';
+import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.45.0/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.31.0/assets',
-  // highlight-i18n-label
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.45.0/assets',
   i18n: {
     en: {
       'libraries.empty-custom-asset-source.label': 'Empty'
-    }
-  },
-  // highlight-i18n-label
-  ui: {
-    elements: {
-      libraries: {
-        // highlight-custom-replace-entries
-        replace: {
-          entries: (defaultEntries, context) => {
-            if (context.selectedBlocks.length !== 1) {
-              return [];
-            }
-
-            const [selectedBlock] = context.selectedBlocks;
-            if (selectedBlock.blockType === 'ly.img.image') {
-              return [
-                ...defaultEntries,
-                {
-                  id: 'empty-custom-asset-source-for-replace',
-                  sourceIds: ['emptySource'],
-                  previewLength: 3,
-                  gridColumns: 3,
-                  gridItemHeight: 'square'
-                }
-              ];
-            }
-
-            return [];
-          }
-        },
-        // highlight-custom-replace-entries
-        insert: {
-          entries: (defaultEntries) => {
-            // highlight-change-default-entries
-            const imageEntry = defaultEntries.find((entry) => {
-              return entry.id === 'ly.img.image';
-            });
-
-            if (imageEntry) {
-              imageEntry.gridColumns = 4;
-            }
-            // highlight-change-default-entries
-
-            return [
-              ...defaultEntries,
-              // highlight-custom-entry
-              {
-                id: 'empty-custom-asset-source',
-                sourceIds: ['emptySource'],
-                previewLength: 3,
-                gridColumns: 3,
-                gridItemHeight: 'square',
-
-                previewBackgroundType: 'contain',
-                gridBackgroundType: 'contain',
-                icon: ({ theme, iconSize }) => {
-                  if (theme === 'dark') {
-                    if (iconSize === 'normal') {
-                      return 'https://img.ly/static/cesdk/guides/icon-normal-dark.svg';
-                    } else {
-                      return 'https://img.ly/static/cesdk/guides/icon-large-dark.svg';
-                    }
-                  }
-
-                  if (iconSize === 'normal') {
-                    return 'https://img.ly/static/cesdk/guides/icon-normal-light.svg';
-                  } else {
-                    return 'https://img.ly/static/cesdk/guides/icon-large-light.svg';
-                  }
-                }
-              },
-              // highlight-custom-entry
-              {
-                id: 'custom-images',
-                sourceIds: ['ly.img.image'],
-                previewLength: 5,
-                gridColumns: 5,
-                icon: 'https://img.ly/static/cesdk/guides/icon-normal-dark.svg'
-              }
-            ];
-          }
-        }
-      }
     }
   },
   callbacks: { onUpload: 'local' } // Enable local uploads in Asset Library.
@@ -113,6 +29,83 @@ CreativeEditorSDK.create('#cesdk_container', config).then(async (instance) => {
       });
     }
   });
+
+  instance.ui.addAssetLibraryEntry({
+    id: 'empty-custom-asset-source',
+    sourceIds: ['emptySource'],
+    previewLength: 3,
+    gridColumns: 3,
+    gridItemHeight: 'square',
+    previewBackgroundType: 'contain',
+    gridBackgroundType: 'contain',
+    icon: ({ theme, iconSize }) => {
+      if (theme === 'dark') {
+        if (iconSize === 'normal') {
+          return 'https://img.ly/static/cesdk/guides/icon-normal-dark.svg';
+        } else {
+          return 'https://img.ly/static/cesdk/guides/icon-large-dark.svg';
+        }
+      }
+
+      if (iconSize === 'normal') {
+        return 'https://img.ly/static/cesdk/guides/icon-normal-light.svg';
+      } else {
+        return 'https://img.ly/static/cesdk/guides/icon-large-light.svg';
+      }
+    }
+  });
+
+  instance.ui.addAssetLibraryEntry({
+    id: 'empty-custom-asset-source-for-replace',
+    sourceIds: ['emptySource'],
+    previewLength: 3,
+    gridColumns: 3,
+    gridItemHeight: 'square'
+  });
+
+  instance.ui.addAssetLibraryEntry({
+    id: 'custom-images',
+    sourceIds: ['ly.img.image'],
+    previewLength: 5,
+    gridColumns: 5,
+    icon: 'https://img.ly/static/cesdk/guides/icon-normal-dark.svg'
+  });
+
+  instance.ui.updateAssetLibraryEntry('ly.img.image', {
+    ...instance.ui.getAssetLibraryEntry('ly.img.image'),
+    gridColumns: 4
+  });
+
+  instance.ui.setReplaceAssetLibraryEntries(
+    ({ defaultEntryIds, selectedBlocks }) => {
+      if (selectedBlocks.length !== 1) {
+        return [];
+      }
+      const [selectedBlock] = selectedBlocks;
+      if (selectedBlock.blockType === '//ly.img.ubq/graphic') {
+        return [...defaultEntryIds, 'empty-custom-asset-source-for-replace'];
+      }
+      return [];
+    }
+  );
+
+  instance.ui.setDockOrder([
+    ...instance.ui.getDockOrder(),
+    {
+      id: 'ly.img.assetLibrary.dock',
+      key: 'empty-custom-asset-source',
+      icon: '@imgly/CustomLibrary',
+      label: 'libraries.empty-custom-asset-source.label',
+      entries: ['empty-custom-asset-source']
+    },
+    {
+      id: 'ly.img.assetLibrary.dock',
+      key: 'custom-images',
+      icon: '@imgly/CustomLibrary',
+      label: 'libraries.custom-images.label',
+      entries: ['custom-images']
+    }
+  ]);
 
   await instance.createDesignScene();
 });
