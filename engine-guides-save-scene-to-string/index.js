9a83d68ce49f770c0d785185aa2bb3d0301ce77c
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-02-19 12:17:59 +0100

    Update examples for v1.45.0

--- engine-guides-save-scene-to-string/index.js
+++ engine-guides-save-scene-to-string/index.js
@@ -1,23 +1,19 @@
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.45.0/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.45.0/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
-  // highlight-save
   engine.scene
     .saveToString()
     .then((sceneAsString) => {
       console.log('Save succeeded');
-      // highlight-result
       console.log(sceneAsString);
-      // highlight-result
     })
     .catch((error) => {
       console.error('Save failed', error);
     });
-  // highlight-save
 });
