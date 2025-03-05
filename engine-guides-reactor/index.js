c90d726f22f10be7e36f49251fa835dc01ae1199
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-03-05 15:58:56 +0100

    Update examples for v1.46.1-rc.0

---
+++ engine-guides-reactor/index.js
@@ -0,0 +1,33 @@
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.46.1-rc.0/index.js';
+
+const config = {
+  license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
+  userId: 'guides-user',
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.46.1-rc.0/assets'
+};
+
+CreativeEngine.init(config).then(async (engine) => {
+  document.getElementById('cesdk_container').append(engine.element);
+
+  await engine.scene.loadFromURL(
+    'https://cdn.img.ly/assets/demo/v1/ly.img.template/templates/cesdk_postcard_1.scene'
+  );
+
+  const reaction = engine.reactor.createReaction();
+
+  const logSelectedElements = () => {
+    reaction.track(() => {
+      console.log('Selected Elements:', engine.block.findAllSelected());
+    });
+  };
+
+  //highlight-subscribe
+  const unsubscribe = reaction.subscribe(logSelectedElements);
+
+  logSelectedElements();
+
+  function cleanup() {
+    unsubscribe();
+    reaction.dispose();
+  }
+});
