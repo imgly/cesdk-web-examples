8f4bb74eb9d242c2fea561226d0988b7aae6c0d7
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-03-21 16:28:49 +0100

    Update examples for v1.48.0-rc.0

--- engine-guides-source-sets/index.js
+++ engine-guides-source-sets/index.js
@@ -1,4 +1,4 @@
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.48.0-rc.0/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
@@ -6,7 +6,6 @@ const config = {
 };
 
 CreativeEngine.init(config).then(async (engine) => {
-  // highlight-setup
   document.getElementById('cesdk_container').append(engine.element);
   const scene = engine.scene.create();
   const page = engine.block.create('page');
@@ -14,9 +13,7 @@ CreativeEngine.init(config).then(async (engine) => {
   engine.block.setHeight(page, 600);
   engine.block.appendChild(scene, page);
   engine.scene.zoomToBlock(page, 50, 50, 50, 50);
-  // highlight-setup
 
-  // highlight-set-source-set
   let block = engine.block.create('graphic');
   engine.block.setShape(block, engine.block.createShape('rect'));
   const imageFill = engine.block.createFill('image');
@@ -40,9 +37,7 @@ CreativeEngine.init(config).then(async (engine) => {
   engine.block.setFill(block, imageFill);
   console.log(engine.block.getSourceSet(imageFill, 'fill/image/sourceSet'));
   engine.block.appendChild(page, block);
-  // highlight-set-source-set
 
-  // highlight-asset-definition
   const assetWithSourceSet = {
     id: 'my-image',
     meta: {
@@ -69,14 +64,10 @@ CreativeEngine.init(config).then(async (engine) => {
       ]
     }
   };
-  // highlight-asset-definition
 
-  // highlight-asset-source
   await engine.asset.addLocalSource('my-dynamic-images');
   engine.asset.addAssetToSource('my-dynamic-images', assetWithSourceSet);
-  // highlight-asset-source
 
-  // highlight-apply-asset
   // Could also acquire the asset using `findAssets` on the source
   const result = await engine.asset.defaultApplyAsset(assetWithSourceSet);
   console.log(
@@ -85,5 +76,19 @@ CreativeEngine.init(config).then(async (engine) => {
       'fill/image/sourceSet'
     )
   ); // Lists the entries from above again.
-  // highlight-apply-asset
+
+  const videoFill = engine.block.createFill('video');
+  engine.block.setSourceSet(videoFill, 'fill/video/sourceSet', [
+    {
+      uri: 'https://img.ly/static/example-assets/sourceset/1x.mp4',
+      width: 1920,
+      height: 1080
+    }
+  ]);
+
+  await engine.block.addVideoFileURIToSourceSet(
+    videoFill,
+    'fill/video/sourceSet',
+    'https://img.ly/static/example-assets/sourceset/2x.mp4'
+  );
 });
