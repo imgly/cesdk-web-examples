8b51f3eb000bf776d79fbfc173e59092f9e99f0d
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-03-27 08:30:37 +0100

    Update examples for v1.48.0-rc.1

--- engine-guides-exporting-blocks/index.js
+++ engine-guides-exporting-blocks/index.js
@@ -1,9 +1,9 @@
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.48.0-rc.1/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.48.0-rc.1/assets'
 };
 
 const exportButton = document.getElementById('export_button');
