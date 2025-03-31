dcf68347f89cec201aba6b16f25c5cfedf8f9b5f
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-03-31 10:37:27 +0200

    Update examples for v1.48.0-rc.3

--- integrate-with-vanilla-js/index.js
+++ integrate-with-vanilla-js/index.js
@@ -1,15 +1,12 @@
-// highlight-1
-import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.31.0/index.js';
+import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.48.0-rc.3/index.js';
 // Import a node module when you work with a bundler:
 // import CreativeEditorSDK from '@cesdk/cesdk-js';
-// highlight-1
 
-// highlight-3
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
   // Serve assets from IMG.LY cdn or locally
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.31.0/assets',
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.48.0-rc.3/assets',
   // Enable local uploads in Asset Library
   callbacks: { onUpload: 'local' }
 };
@@ -20,13 +17,8 @@ CreativeEditorSDK.create('#cesdk_container', config).then(async (editor) => {
   editor.addDefaultAssetSources();
   editor.addDemoAssetSources({ sceneMode: 'Design' });
   await editor.createDesignScene();
-  // highlight-engine
   const engine = editor.engine; // Access the engine directly if required
-  // highlight-engine
 
-  // highlight-dispose
   // Dispose the Editor when done to cleanup all memories and dangling references
   // editor.dispose();
-  // highlight-dispose
 });
-// highlight-3
