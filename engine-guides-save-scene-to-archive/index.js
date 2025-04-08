fd23fabc3bcf7347c631084b3a898efff2821d0d
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-04-08 17:03:37 +0200

    Update examples for v1.49.0-rc.0

--- engine-guides-save-scene-to-archive/index.js
+++ engine-guides-save-scene-to-archive/index.js
@@ -1,27 +1,23 @@
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.49.0-rc.0/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.49.0-rc.0/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
-  // highlight-save
   engine.scene
     .saveToArchive()
     .then((blob) => {
-      // highlight-create-form-data
       const formData = new FormData();
       formData.append('file', blob);
       fetch('/upload', {
         method: 'POST',
         body: formData
       });
-      // highlight-create-form-data
     })
     .catch((error) => {
       console.error('Save failed', error);
     });
-  // highlight-save
 });
