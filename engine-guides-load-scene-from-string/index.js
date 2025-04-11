19ffda248788a50654d3b9783c9923dd9b12d8e5
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-04-11 12:32:22 +0200

    Update examples for v1.48.1-rc.0

--- engine-guides-load-scene-from-string/index.js
+++ engine-guides-load-scene-from-string/index.js
@@ -1,38 +1,32 @@
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.48.1-rc.0/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.48.1-rc.0/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
   // Load default assets referenced in the scene
   engine.addDefaultAssetSources();
 
-  // highlight-fetch-string
   const sceneUrl =
     'https://cdn.img.ly/assets/demo/v1/ly.img.template/templates/cesdk_postcard_1.scene';
   const sceneString = await fetch(sceneUrl).then((response) => {
     return response.text();
   });
-  // highlight-fetch-string
 
-  // highlight-load
   let scene = await engine.scene
     .loadFromString(sceneString)
     .then(() => {
       console.log('Load succeeded');
 
-      // highlight-set-text-dropshadow
       let text = engine.block.findByType('text')[0];
       engine.block.setDropShadowEnabled(text, true);
-      // highlight-set-text-dropshadow
     })
     .catch((error) => {
       console.error('Load failed', error);
     });
-  // highlight-load
 
   // Attach engine canvas to DOM
   document.getElementById('cesdk_container').append(engine.element);
--- engine-guides-save-scene-to-blob/index.js
+++ engine-guides-save-scene-to-blob/index.js
@@ -1,33 +1,27 @@
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.48.1-rc.0/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.48.1-rc.0/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
-  // highlight-save
   engine.scene
     .saveToString()
     .then((savedSceneString) => {
-      // highlight-create-blob
       const blob = new Blob([savedSceneString], {
         type: 'text/plain'
       });
-      // highlight-create-blob
 
-      // highlight-create-form-data
       const formData = new FormData();
       formData.append('file', blob);
       fetch('/upload', {
         method: 'POST',
         body: formData
       });
-      // highlight-create-form-data
     })
     .catch((error) => {
       console.error('Save failed', error);
     });
-  // highlight-save
 });
