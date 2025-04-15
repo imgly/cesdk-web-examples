ee8e4c16a4a0d6b8d3378439f17d70a8c1e858fa
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-04-15 20:13:06 +0200

    Update examples for v1.49.0-rc.3

--- engine-guides-custom-template-source/index.js
+++ engine-guides-custom-template-source/index.js
@@ -1,55 +1,21 @@
-import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.31.0/index.js';
+import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.49.0-rc.3/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.31.0/assets',
-  // highlight-ui-config
-  ui: {
-    elements: {
-      dock: {
-        groups: [
-          {
-            id: 'my-templates-group',
-            entryIds: ['my-templates-entry']
-          }
-        ]
-      },
-      libraries: {
-        insert: {
-          entries: [
-            {
-              id: 'my-templates-entry',
-              sourceIds: ['my-templates'],
-              title: 'My Templates'
-            }
-          ]
-        }
-      }
-    }
-  },
-  // highlight-ui-config
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.49.0-rc.3/assets',
   callbacks: { onUpload: 'local' } // Enable local uploads in Asset Library.
 };
 
 CreativeEditorSDK.create('#cesdk_container', config).then(async (instance) => {
-  // highlight-demo
   instance.addDemoAssetSources({ sceneMode: 'Design' });
-  // highlight-demo
-  // highlight-add-local-source
   instance.engine.asset.addLocalSource(
-    // highlight-sourceid
     'my-templates',
     undefined,
-    // highlight-sourceid
-    // highlight-applyasset
     function applyAsset(asset) {
       instance.engine.scene.applyTemplateFromURL(asset.meta.uri);
     }
-    // highlight-applyasset
   );
-  // highlight-add-local-source
-  // highlight-add-asset
   instance.engine.asset.addAssetToSource('my-templates', {
     id: 'template1',
     label: 'Template 1',
@@ -58,6 +24,23 @@ CreativeEditorSDK.create('#cesdk_container', config).then(async (instance) => {
       thumbUri: `${window.location.protocol}//${window.location.host}/template_thumb.png`
     }
   });
-  // highlight-add-asset
+  instance.ui.addAssetLibraryEntry({
+    id: 'my-templates-entry',
+    sourceIds: ['my-templates'],
+    sceneMode: 'Design',
+    previewLength: 5,
+    previewBackgroundType: 'cover',
+    gridBackgroundType: 'cover',
+    gridColumns: 3
+  });
+  instance.ui.setDockOrder([
+    {
+      id: 'ly.img.assetLibrary.dock',
+      key: 'my-templates-dock-entry',
+      label: 'My Templates',
+      icon: '@imgly/Template',
+      entries: ['my-templates-entry']
+    }
+  ]);
   instance.createDesignScene();
 });
--- engine-guides-uri-resolver/index.js
+++ engine-guides-uri-resolver/index.js
@@ -1,18 +1,15 @@
-import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.31.0/index.js';
+import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.49.0-rc.3/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.31.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.49.0-rc.3/assets'
 };
 
 CreativeEditorSDK.create('#cesdk_container', config).then((instance) => {
-  // highlight-get-absolute-base-path
-  /** This will return 'https://cdn.img.ly/packages/imgly/cesdk-js/1.31.0/assets/banana.jpg'. */
+  /** This will return 'https://cdn.img.ly/packages/imgly/cesdk-js/1.49.0-rc.3/assets/banana.jpg'. */
   instance.engine.editor.getAbsoluteURI('/banana.jpg');
-  // highlight-get-absolute-base-path
 
-  // highlight-resolver
   /** Replace all .jpg files with the IMG.LY logo! **/
   instance.engine.editor.setURIResolver((uri, defaultURIResolver) => {
     if (uri.endsWith('.jpg')) {
@@ -21,9 +18,7 @@ CreativeEditorSDK.create('#cesdk_container', config).then((instance) => {
     /** Make use of the default URI resolution behavior. */
     return defaultURIResolver(uri);
   });
-  // highlight-resolver
 
-  // highlight-get-absolute-custom
   /**
    * The custom resolver will return a path to the IMG.LY logo because the given path ends with '.jpg'.
    * This applies regardless if the given path is relative or absolute.
@@ -35,13 +30,10 @@ CreativeEditorSDK.create('#cesdk_container', config).then((instance) => {
 
   /** Because a custom resolver is set, relative paths that the resolver does not transform remain unmodified! */
   instance.engine.editor.getAbsoluteURI('/orange.png');
-  // highlight-get-absolute-custom
 
-  // highlight-remove-resolver
   /** Removes the previously set resolver. */
   instance.engine.editor.setURIResolver(null);
 
-  /** Since we've removed the custom resolver, this will return 'https://cdn.img.ly/packages/imgly/cesdk-js/1.31.0/assets/banana.jpg' like before. */
+  /** Since we've removed the custom resolver, this will return 'https://cdn.img.ly/packages/imgly/cesdk-js/1.49.0-rc.3/assets/banana.jpg' like before. */
   instance.engine.editor.getAbsoluteURI('/banana.jpg');
-  // highlight-remove-resolver
 });
