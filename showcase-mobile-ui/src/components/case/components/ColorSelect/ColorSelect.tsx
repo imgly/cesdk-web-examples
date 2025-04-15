5922afbe325a05d65896b1bd94f383068849bd13
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-04-15 20:13:06 +0200

    Update examples for v1.49.0-rc.3

--- showcase-mobile-ui/src/components/case/components/ColorSelect/ColorSelect.tsx
+++ showcase-mobile-ui/src/components/case/components/ColorSelect/ColorSelect.tsx
@@ -3,8 +3,6 @@ import { HexColorPicker } from 'react-colorful';
 import classes from './ColorSelect.module.css';
 import { RGBAColor } from '@cesdk/engine';
 import { hexToRgba, isColorEqual, rgbaToHex } from '../../lib/ColorUtilities';
-import useDebounceCallback from './UseDebounceCallback';
-import { useEditor } from '../../EditorContext';
 
 const ALL_COLORS = [
   '#ffffff',
@@ -22,21 +20,13 @@ const ColorSelect = ({
   onClick: (color: RGBAColor) => void;
   activeColor?: RGBAColor;
 }) => {
-  const { engine } = useEditor();
-
-  const debouncedChangeHandler = useDebounceCallback(() => engine.editor.addUndoStep(), 200);
-  const handleChange = (color: string | RGBAColor) => {
-    onClick(typeof color === 'string' ? hexToRgba(color) : color);
-    debouncedChangeHandler();
-  };
-
   return (
     <div className={classes.wrapper}>
       <div className={classes.colorPalette}>
         {ALL_COLORS.map((color) => (
           <button
             key={rgbaToHex(color)}
-            onClick={() => handleChange(color)}
+            onClick={() => onClick(color)}
             style={{ backgroundColor: rgbaToHex(color) }}
             className={classNames(classes.colorButton, {
               [classes['colorButton--active']]: isColorEqual(color, activeColor)
@@ -48,7 +38,7 @@ const ColorSelect = ({
       <HexColorPicker
         className={classes.colorPicker}
         color={rgbaToHex(activeColor)}
-        onChange={handleChange}
+        onChange={(colorHex) => onClick(hexToRgba(colorHex))}
       />
     </div>
   );
