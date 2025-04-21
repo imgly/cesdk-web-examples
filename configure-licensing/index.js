5f82aa08e17514fb2a979dacbdcba024c8dc052c
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-04-21 19:43:30 +0200

    Update examples for v1.50.0-rc.0

--- configure-licensing/index.js
+++ configure-licensing/index.js
@@ -1,11 +1,9 @@
-import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.31.0/index.js';
+import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.50.0-rc.0/index.js';
 
-// highlight-config
 let config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user' // Replace with your license key.
 };
-// highlight-config
 
 CreativeEditorSDK.create('#cesdk_container', config).then(async (instance) => {
   await instance.createDesignScene();
--- engine-guides-create-scene-from-image-canvas/index.js
+++ engine-guides-create-scene-from-image-canvas/index.js
@@ -1,4 +1,4 @@
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.50.0-rc.0/index.js';
 
 // Draw the text 'img.ly' to the demo canvas
 const canvas = document.getElementById('my-canvas');
@@ -6,30 +6,22 @@ const ctx = canvas.getContext('2d');
 ctx.font = '100px Arial';
 ctx.fillText('img.ly', 120, 270);
 
-// highlight-dataURL
 const dataURL = canvas.toDataURL();
-// highlight-dataURL
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.50.0-rc.0/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
-  // highlight-initialImageURL
   let scene = await engine.scene.createFromImage(dataURL);
-  // highlight-initialImageURL
 
-  // highlight-find-block
   // Find the automatically added graphic block with an image fill in the scene.
   const block = engine.block.findByType('graphic')[0];
-  // highlight-find-block
 
-  // highlight-set-opacity
   // Change its opacity.
   engine.block.setOpacity(block, 0.5);
-  // highlight-set-opacity
 
   // Attach engine canvas to DOM
   document.getElementById('cesdk_container').append(engine.element);
