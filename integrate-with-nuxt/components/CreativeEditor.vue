ba0aa7ab01699a36813748f662e4d8eb5c029657
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-05-08 10:35:20 +0200

    Update examples for v1.51.0-rc.1

---
+++ integrate-with-nuxt/components/CreativeEditor.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="cesdk_container" style="width: 100vw; height: 100vh;"></div>
+</template>
+
+<script>
+export default {
+  name: 'CreativeEditor',
+  mounted() {
+    import('https://cdn.img.ly/packages/imgly/cesdk-js/1.51.0-rc.1/index.js')
+      .then(async ({ default: CreativeEditorSDK }) => {
+        const config = {
+          license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
+          userId: 'guides-user',
+          baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.51.0-rc.1/assets',
+          callbacks: { onUpload: 'local' }
+        };
+
+        const instance = await CreativeEditorSDK.create('#cesdk_container', config);
+        await instance.addDefaultAssetSources();
+        await instance.addDemoAssetSources({ sceneMode: 'Design' });
+        await instance.createDesignScene();
+      });
+  }
+};
+</script>
