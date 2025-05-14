83231dddc5991ae3d1c21d49b2a7ec0ecc20e4f1
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-05-14 16:40:37 +0200

    Update examples for v1.51.0-rc.3

---
+++ integrate-with-next-js-headless-ui/app/page.js
@@ -0,0 +1,14 @@
+// other imports...
+import { default as CustomEditor } from "./components/CustomEditorNoSSR";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        {/* other components.. */}
+        <CustomEditor />
+        {/* other components.. */}
+      </main>
+    </div>
+  );
+}
