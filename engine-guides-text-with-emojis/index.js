b7e50b1763c1c1e0c375cdae66d4a3f77bc56b8f
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-05-26 15:21:05 +0200

    Update examples for v1.52.0-rc.1

--- engine-guides-text-with-emojis/index.js
+++ engine-guides-text-with-emojis/index.js
@@ -1,16 +1,14 @@
-// highlight-setup
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.52.0-rc.1/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.52.0-rc.1/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
   document.getElementById('cesdk_container').append(engine.element);
 
-  // highlight-change-default-emoji-font
   let uri = engine.editor.getSettingString('ubq://defaultEmojiFontFileUri');
   // From a bundle
   engine.editor.setSettingString(
@@ -22,7 +20,6 @@ CreativeEngine.init(config).then(async (engine) => {
     'ubq://defaultEmojiFontFileUri',
     'https://cdn.img.ly/assets/v2/emoji/NotoColorEmoji.ttf'
   );
-  // highlight-change-default-emoji-font
 
   const scene = engine.scene.create();
 
@@ -33,11 +30,9 @@ CreativeEngine.init(config).then(async (engine) => {
 
   engine.scene.zoomToBlock(page, 40, 40, 40, 40);
 
-  // highlight-add-text-with-emoji
   const text = engine.block.create('text');
   engine.block.setString(text, 'text/text', 'Text with an emoji 🧐');
   engine.block.setWidthMode(text, 'Auto');
   engine.block.setHeightMode(text, 'Auto');
   engine.block.appendChild(page, text);
-  // highlight-add-text-with-emoji
 });
---
+++ integrate-with-nodejs/index.js
@@ -0,0 +1,34 @@
+import fs from 'fs/promises';
+import CreativeEngine from '@cesdk/node';
+
+const { MimeType } = CreativeEngine;
+
+// Configuration for the engine
+const config = {
+  license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
+  userId: 'guides-user',
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-node/1.52.0-rc.1/assets'
+};
+
+CreativeEngine.init(config).then(async (engine) => {
+  console.log('CE.SDK Engine initialized');
+
+  try {
+    await engine.addDefaultAssetSources();
+    await engine.scene.loadFromURL(
+      'https://cdn.img.ly/assets/demo/v1/ly.img.template/templates/cesdk_instagram_photo_1.scene'
+    );
+
+    const [page] = engine.block.findByType('page');
+
+    const blob = await engine.block.export(page, MimeType.Png);
+    const arrayBuffer = await blob.arrayBuffer();
+
+    await fs.writeFile('./example-output.png', Buffer.from(arrayBuffer));
+    console.log('Export completed: example-output.png');
+  } catch (error) {
+    console.error('Error processing scene:', error);
+  } finally {
+    engine.dispose();
+  }
+});
