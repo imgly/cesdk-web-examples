09be087856df11f547d22b146d5b1c7ae2e71dd3
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-05-29 13:06:34 +0200

    Update examples for v1.52.0-rc.2

--- engine-guides-scopes/index.js
+++ engine-guides-scopes/index.js
@@ -1,10 +1,9 @@
-// highlight-setup
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.52.0-rc.2/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.52.0-rc.2/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
@@ -12,13 +11,9 @@ CreativeEngine.init(config).then(async (engine) => {
     'https://img.ly/static/ubq_samples/imgly_logo.jpg'
   );
   const block = engine.block.findByType('graphic')[0];
-  // highlight-setup
 
-  // highlight-findAllScopes
   const scopes = engine.editor.findAllScopes();
-  // highlight-findAllScopes
 
-  // highlight-setGlobalScope
   /* Let the global scope defer to the block-level. */
   engine.editor.setGlobalScope('layer/move', 'Defer');
 
@@ -28,14 +23,10 @@ CreativeEngine.init(config).then(async (engine) => {
   } catch (err) {
     console.log(err.message);
   }
-  // highlight-setGlobalScope
 
-  // highlight-getGlobalScope
   /* This will return 'Defer'. */
   engine.editor.getGlobalScope('layer/move');
-  // highlight-getGlobalScope
 
-  // highlight-setScopeEnabled
   /* Allow the user to control the layout properties of the image block. */
   engine.block.setScopeEnabled(block, 'layer/move', true);
 
@@ -45,17 +36,12 @@ CreativeEngine.init(config).then(async (engine) => {
   } catch (err) {
     console.log(err.message);
   }
-  // highlight-setScopeEnabled
 
-  // highlight-isScopeEnabled
   /* Verify that the 'layer/move' scope is now enabled for the image block. */
   engine.block.isScopeEnabled(block, 'layer/move');
-  // highlight-isScopeEnabled
 
-  // highlight-isAllowedByScope
   /* This will return true as well since the global scope is set to 'Defer'. */
   engine.block.isAllowedByScope(block, 'layer/move');
-  // highlight-isAllowedByScope
 
   // Attach engine canvas to DOM
   document.getElementById('cesdk_container').append(engine.element);
