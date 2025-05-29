bb3f45411cd1bccd0ad328b19fc617048647e1a7
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-05-29 13:06:34 +0200

    Update examples for v1.52.0-rc.2

--- engine-guides-create-scene-from-image-url/index.js
+++ engine-guides-create-scene-from-image-url/index.js
@@ -1,26 +1,21 @@
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.52.0-rc.2/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.52.0-rc.2/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
-  // highlight-createFromImage
   await engine.scene.createFromImage(
     'https://img.ly/static/ubq_samples/sample_4.jpg'
   );
 
-  // highlight-findByType
   // Find the automatically added graphic block in the scene that contains the image fill.
   const block = engine.block.findByType('graphic')[0];
-  // highlight-findByType
 
-  // highlight-setOpacity
   // Change its opacity.
   engine.block.setOpacity(block, 0.5);
-  // highlight-setOpacity
 
   // Attach engine canvas to DOM
   document.getElementById('cesdk_container').append(engine.element);
---
+++ integrate-with-sveltekit/src/lib/CreativeEditorSDK.svelte
@@ -0,0 +1,77 @@
+<script>
+  import CreativeEditorSDK from '@cesdk/cesdk-js';
+  import { onDestroy, onMount } from 'svelte';
+
+  // reference to the container HTML element where CE.SDK will be initialized
+  let container;
+  // where to keep track of the CE.SDK instance
+  let cesdk = null;
+
+  // deafult CreativeEditor SDK configuration
+  const defaultConfig = {
+    license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu', // replace it with your license key
+    userId: 'guides-user',
+    baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.52.0-rc.2/assets',
+    callbacks: { onUpload: 'local' } // enable local file uploads in the Asset Library
+    // other default configs...
+  };
+
+  // accessing the component's props
+  const { el, children, class: _, config, ...props } = $props();
+
+  // hook to initialize the CreativeEditorSDK component
+  onMount(() => {
+    // integrate the configs read from props with the default ones
+    const ceSDKConfig = {
+      ...defaultConfig,
+      ...config
+    };
+
+    try {
+      // initialize the CreativeEditorSDK instance in the container element
+      // using the given config
+      CreativeEditorSDK.create(container, ceSDKConfig).then(
+        async (instance) => {
+          cesdk = instance;
+
+          // do something with the instance of CreativeEditor SDK (e.g., populate
+          // the asset library with default / demo asset sources)
+          await Promise.all([
+            cesdk.addDefaultAssetSources(),
+            cesdk.addDemoAssetSources({ sceneMode: 'Design' })
+          ]);
+
+          // create a new design scene in the editor
+          await cesdk.createDesignScene();
+        }
+      );
+    } catch (err) {
+      console.warn(`CreativeEditor SDK failed to mount.`, { err });
+    }
+  });
+
+  // hook to clean up when the component unmounts
+  onDestroy(() => {
+    try {
+      // dispose of the CE.SDK instance if it exists
+      if (cesdk) {
+        cesdk.dispose();
+        cesdk = null;
+      }
+    } catch (err) {
+      // log error if CreativeEditor SDK fails to unmount
+      console.warn(`CreativeEditor SDK failed to unmount.`, { err });
+    }
+  });
+</script>
+
+<!-- the container HTML element where the CE.SDK editor will be mounted -->
+<div id="cesdk_container" bind:this={container}></div>
+
+<style>
+  /* styling for the CE.SDK container element to take full viewport size */
+  #cesdk_container {
+    height: 100vh;
+    width: 100vw;
+  }
+</style>
