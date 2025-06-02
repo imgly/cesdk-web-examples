86793e5224ada801e3ed2cc739149e78dca6d5bb
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-06-02 18:54:43 +0200

    Update examples for v1.53.0-rc.0

--- engine-guides-store-metadata/index.js
+++ engine-guides-store-metadata/index.js
@@ -1,10 +1,9 @@
-// highlight-setup
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.53.0-rc.0/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.31.0/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.53.0-rc.0/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
@@ -15,9 +14,7 @@ CreativeEngine.init(config).then(async (engine) => {
   // Note: In this example we know that the first block since there is only one block in the scene
   // at this point. You might need to filter by fill type if you have multiple blocks.
   const block = engine.block.findByType('graphic')[0];
-  // highlight-setup
 
-  // highlight-setMetadata
   engine.block.setMetadata(scene, 'author', 'img.ly');
   engine.block.setMetadata(block, 'customer_id', '1234567890');
 
@@ -28,29 +25,21 @@ CreativeEngine.init(config).then(async (engine) => {
     received: true
   };
   engine.block.setMetadata(block, 'payment', JSON.stringify(payment));
-  // highlight-setMetadata
 
-  // highlight-getMetadata
   /* This will return 'img.ly' */
   engine.block.getMetadata(scene, 'author');
 
   /* This will return '1000000' */
   engine.block.getMetadata(block, 'customer_id');
-  // highlight-getMetadata
 
-  // highlight-findAllMetadata
   /* This will return ['customer_id'] */
   engine.block.findAllMetadata(block);
-  // highlight-findAllMetadata
 
-  // highlight-removeMetadata
   engine.block.removeMetadata(block, 'payment');
 
   /* This will return false */
   engine.block.hasMetadata(block, 'payment');
-  // highlight-removeMetadata
 
-  // highlight-persistence
   /* We save our scene and reload it from scratch */
   const sceneString = await engine.scene.saveToString();
   scene = await engine.scene.loadFromString(sceneString);
@@ -60,7 +49,6 @@ CreativeEngine.init(config).then(async (engine) => {
 
   /* And this still returns '1234567890' */
   engine.block.getMetadata(block, 'customer_id');
-  // highlight-persistence
 
   // Attach engine canvas to DOM
   document.getElementById('cesdk_container').append(engine.element);
---
+++ integrate-with-next-js/app/components/CreativeEditorSDK.js
@@ -0,0 +1,77 @@
+'use client';
+
+import CreativeEditorSDK from '@cesdk/cesdk-js';
+import { useEffect, useRef, useState } from 'react';
+
+// configure CreativeEditor SDK
+const config = {
+  license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
+  userId: 'guides-user',
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.53.0-rc.0/assets', // replace with a valid CE.SDK license key
+  callbacks: { onUpload: 'local' } // enable local file uploads in the Asset Library
+};
+
+export default function CreativeEditorSDKComponent() {
+  // reference to the container HTML element where CESDK will be initialized
+  const cesdk_container = useRef(null);
+  // define a state variable to keep track of the CESDK instance
+  const [cesdk, setCesdk] = useState(null);
+
+  useEffect(() => {
+    // prevent initialization if the container element is not available yet
+    if (!cesdk_container.current) {
+      return;
+    }
+
+    // flag to keep track of the component unmount
+    let cleanedUp = false;
+    // where to store the local CE.SDK instance
+    let instance;
+
+    // initialize the CreativeEditorSDK instance in the container HTML element
+    // using the given config
+    CreativeEditorSDK.create(cesdk_container.current, config).then(
+      async (_instance) => {
+        // assign the current CD.SDK instance to the local variable
+        instance = _instance;
+
+        if (cleanedUp) {
+          instance.dispose();
+          return;
+        }
+
+        // do something with the instance of CreativeEditor SDK (e.g., populate
+        // the asset library with default / demo asset sources)
+        await Promise.all([
+          instance.addDefaultAssetSources(),
+          instance.addDemoAssetSources({ sceneMode: 'Design' })
+        ]);
+
+        // create a new design scene in the editor
+        await instance.createDesignScene();
+
+        // store the CESDK instance in state
+        setCesdk(instance);
+      }
+    );
+
+    // cleanup function to dispose of the CESDK instance when the component unmounts
+    const cleanup = () => {
+      // clear the local state and dispose of the CS.SDK instance (if it has been assigned)
+      cleanedUp = true;
+      instance?.dispose();
+      setCesdk(null);
+    };
+
+    // to ensure cleanup runs when the component unmounts
+    return cleanup;
+  }, [cesdk_container]);
+
+  return (
+    // the container HTML element where the CESDK editor will be mounted
+    <div
+      ref={cesdk_container}
+      style={{ width: '100vw', height: '100vh' }}
+    ></div>
+  );
+}
