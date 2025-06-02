439e4a706f2522d7055b148cfca6f82a2e9b24bf
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-06-02 18:54:43 +0200

    Update examples for v1.53.0-rc.0

---
+++ integrate-with-vue-headless-ui/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+// other imports...
+import CustomEditor from "./components/CustomEditor.vue"
+// other imports...
+</script>
+
+<template>
+  <main>
+    <!-- other components... -->
+    <CustomEditor />
+    <!-- other components... -->
+  </main>
+</template>
+
+<style scoped>
+/* your style... */
+</style>
--- integrate-with-vue/src/App.vue
+++ integrate-with-vue/src/App.vue
@@ -1,14 +1,24 @@
-<script setup lang="ts">
-import CreativeEditor from './components/CreativeEditor.vue';
-</script>
-
-
 <template>
-
-  <CreativeEditor :config="{
-    license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
-    userId: 'guides-user',
-    callbacks: { onUpload: 'local' } // Enables local uploads in Asset Library. 
-  }" />
-
+  <CreativeEditor :config="editorConfig" />
 </template>
+
+<script>
+import CreativeEditor from './components/CreativeEditor.vue';
+
+export default {
+  name: 'App',
+  components: { CreativeEditor },
+  data() {
+    return {
+      editorConfig: {
+        license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
+        userId: 'guides-user',
+        baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.53.0-rc.0/assets',
+        callbacks: {
+          onUpload: 'local'
+        }
+      }
+    };
+  }
+};
+</script>
