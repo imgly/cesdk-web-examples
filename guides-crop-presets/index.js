a5d432bfd2b2ca7a67bba58604b873ca5548bfa2
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-06-10 15:28:17 +0200

    Update examples for v1.53.0-rc.2

---
+++ guides-crop-presets/index.js
@@ -0,0 +1,98 @@
+import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.53.0-rc.2/index.js';
+
+const config = {
+  license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.53.0-rc.2/assets',
+  userId: 'guides-user',
+  ui: {
+    scale: 'normal',
+    stylesheets: {
+      /* ... */
+    },
+    elements: {
+      /* ... */
+    },
+    pagePresetLibraries: [
+      // 'ly.img.crop.presets',
+      'my-custom-crop-presets'
+    ]
+  },
+  callbacks: { onUpload: 'local' } // Enable local uploads in Asset Library.
+};
+
+CreativeEditorSDK.create('#cesdk_container', config).then(async (instance) => {
+  // Do something with the instance of CreativeEditor SDK
+  // Populate the asset library with default / demo asset sources.
+  instance.addDefaultAssetSources();
+  instance.addDemoAssetSources({ sceneMode: 'Design' });
+
+  // Add a custom crop preset asset source.
+  instance.engine.asset.addLocalSource('my-custom-crop-presets');
+
+  instance.engine.asset.addAssetToSource(
+    'my-custom-crop-presets',
+    {
+      id: 'aspect-ratio-free',
+      label: {
+        en: 'Free'
+      },
+      meta: {
+        width: 80,
+        height: 120,
+        thumbUri: `${window.location.protocol}//${window.location.host}/ratio-free.png`
+      },
+      payload: {
+        transformPreset: {
+          type: 'FreeAspectRatio'
+        }
+      }
+    }
+  );
+
+  instance.engine.asset.addAssetToSource(
+    'my-custom-crop-presets',
+    {
+      id: 'aspect-ratio-16-9',
+      label: {
+        en: '16:9'
+      },
+      meta: {
+        width: 80,
+        height: 120,
+        thumbUri: `${window.location.protocol}//${window.location.host}/ratio-16-9.png`
+      },
+      payload: {
+        transformPreset: {
+          type: 'FixedAspectRatio',
+          width: 16,
+          height: 9
+        }
+      }
+    }
+  );
+
+  instance.engine.asset.addAssetToSource(
+    'my-custom-crop-presets',
+    {
+      id: 'din-a1-portrait',
+      label: {
+        en: 'DIN A1 Portrait'
+      },
+      meta: {
+        width: 80,
+        height: 120,
+        thumbUri: `${window.location.protocol}//${window.location.host}/din-a1-portrait.png`
+      },
+      payload: {
+        transformPreset: {
+          type: 'FixedSize',
+          width: 594,
+          height: 841,
+          designUnit: 'Millimeter'
+        }
+      }
+    }
+  );
+
+  await instance.createDesignScene();
+});
