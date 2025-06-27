26c34e1d7dcf95e3e87ae1e5b0d7301a3d0a236d
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-06-27 16:32:13 +0200

    Update examples for v1.55.0-rc.0

--- integrate-with-next-js-headless-ui/app/components/CustomEditor.js
+++ integrate-with-next-js-headless-ui/app/components/CustomEditor.js
@@ -17,7 +17,7 @@ export default function CustomEditor() {
       license:
         'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu', // replace it with your license key
       userId: 'guides-user',
-      baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.53.0/assets'
+      baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.55.0-rc.0/assets'
     };
 
     // initialize CreativeEngine in headless mode
