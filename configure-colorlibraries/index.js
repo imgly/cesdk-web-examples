86f20abd34c1531d159f62c15a834fd6e99452cf
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-07-18 14:50:16 +0200

    Update examples for v1.55.2

--- configure-colorlibraries/index.js
+++ configure-colorlibraries/index.js
@@ -1,4 +1,4 @@
-import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.53.0/index.js';
+import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.55.2/index.js';
 
 const colors = [
   {
