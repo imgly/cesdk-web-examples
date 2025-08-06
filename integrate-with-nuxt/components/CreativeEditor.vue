cf9b355d2b62f45618c941f8f1a78bdda9b9f4a9
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-08-06 09:59:25 +0200

    Update examples for v1.57.0

--- integrate-with-nuxt/components/CreativeEditor.vue
+++ integrate-with-nuxt/components/CreativeEditor.vue
@@ -6,12 +6,12 @@
 export default {
   name: 'CreativeEditor',
   mounted() {
-    import('https://cdn.img.ly/packages/imgly/cesdk-js/1.56.0/index.js')
+    import('https://cdn.img.ly/packages/imgly/cesdk-js/1.57.0/index.js')
       .then(async ({ default: CreativeEditorSDK }) => {
         const config = {
           license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
           userId: 'guides-user',
-          baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.56.0/assets',
+          baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.57.0/assets',
           callbacks: { onUpload: 'local' }
         };
 
