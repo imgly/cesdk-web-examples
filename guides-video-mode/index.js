bccf32ea64742b9948c885263cebad825ccee67c
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-09-08 08:49:29 +0200

    Update examples for v1.59.1

--- guides-video-mode/index.js
+++ guides-video-mode/index.js
@@ -1,11 +1,11 @@
-import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.59.0/index.js';
+import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.59.1/index.js';
 
 window.onload = async () => {
   const config = {
     license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
     userId: 'guides-user',
     theme: 'light',
-    baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.59.0/assets',
+    baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.59.1/assets',
     ui: {
       elements: {
         view: 'default',
