91931549e491db9320924d2650c6a60ca334b974
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-09-17 14:51:11 +0200

    Update examples for v1.60.0-rc.3

--- engine-guides-create-scene-from-scratch/index.js
+++ engine-guides-create-scene-from-scratch/index.js
@@ -1,9 +1,9 @@
-import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.59.1/index.js';
+import CreativeEngine from 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.60.0-rc.3/index.js';
 
 const config = {
   license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
   userId: 'guides-user',
-  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.59.1/assets'
+  baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-engine/1.60.0-rc.3/assets'
 };
 
 CreativeEngine.init(config).then(async (engine) => {
@@ -20,4 +20,3 @@ CreativeEngine.init(config).then(async (engine) => {
   // Attach engine canvas to DOM
   document.getElementById('cesdk_container').append(engine.element);
 });
-
--- guides-video-mode/index.js
+++ guides-video-mode/index.js
@@ -1,11 +1,11 @@
-import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.59.1/index.js';
+import CreativeEditorSDK from 'https://cdn.img.ly/packages/imgly/cesdk-js/1.60.0-rc.3/index.js';
 
 window.onload = async () => {
   const config = {
     license: 'vERESgSXbYj5Rs-FF4DzkMvhdQLh0Mxe6AD8V-doP6wqe_gmYmx_oUKqIlMkwpMu',
     userId: 'guides-user',
     theme: 'light',
-    baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.59.1/assets',
+    baseURL: 'https://cdn.img.ly/packages/imgly/cesdk-js/1.60.0-rc.3/assets',
     ui: {
       elements: {
         view: 'default',
@@ -13,47 +13,30 @@ window.onload = async () => {
           settings: true
         },
         navigation: {
-          position: 'top',
-          action: {
-            save: true,
-            load: true,
-            download: true,
-            export: true
-          }
-        },
+          position: 'top'
+        }
       }
-    },
-    callbacks: {
-      onUpload: 'local',
-      onSave: (scene) => {
-        const element = document.createElement('a');
-        const base64Data = btoa(unescape(encodeURIComponent(scene)));
-        element.setAttribute(
-          'href',
-          `data:application/octet-stream;base64,${base64Data}`
-        );
-        element.setAttribute(
-          'download',
-          `cesdk-${new Date().toISOString()}.scene`
-        );
-
-        element.style.display = 'none';
-        document.body.appendChild(element);
-
-        element.click();
-
-        document.body.removeChild(element);
-      },
-      onLoad: 'upload',
-      onDownload: 'download',
-      onExport: 'download'
     }
   };
 
   const cesdk = await CreativeEditorSDK.create('#cesdk_container', config);
+
+  // Configure navigation bar actions using the new API
+  cesdk.ui.insertNavigationBarOrderComponent('last', {
+    id: 'ly.img.actions.navigationBar',
+    children: [
+      'ly.img.saveScene.navigationBar',
+      'ly.img.importScene.navigationBar',
+      'ly.img.exportScene.navigationBar',
+      'ly.img.exportVideo.navigationBar'
+    ]
+  });
+
   cesdk.addDefaultAssetSources();
-  cesdk.addDemoAssetSources({ sceneMode: 'Video' });
-  cesdk.ui.setBackgroundTrackAssetLibraryEntries(['ly.img.image', 'ly.img.video']);
+  cesdk.addDemoAssetSources({ sceneMode: 'Video', withUploadAssetSources: true });
+  cesdk.ui.setBackgroundTrackAssetLibraryEntries([
+    'ly.img.image',
+    'ly.img.video'
+  ]);
   const scene = await cesdk.createVideoScene();
 };
-
