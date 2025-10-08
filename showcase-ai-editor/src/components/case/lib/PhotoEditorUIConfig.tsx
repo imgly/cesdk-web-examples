5e18298d618375881fb027dc669e006d40caafaa
hiyeonKim <hiyeon.kim@img.ly>
2025-10-08 16:43:30 +0200

    Update Showcases

--- showcase-ai-editor/src/components/case/lib/PhotoEditorUIConfig.tsx
+++ showcase-ai-editor/src/components/case/lib/PhotoEditorUIConfig.tsx
@@ -1,13 +1,12 @@
 import CreativeEditorSDK, {
   AssetResult,
-  CreativeEngine,
-  SettingsBool
+  CreativeEngine
 } from '@cesdk/cesdk-js';
 import { removeBackground } from '@imgly/background-removal';
 import APP_ASSETS from '../assets/Apps.json';
+import { caseAssetPath } from '../util/photo-util';
 import { getImageSize } from './CreativeEngineUtils';
 import loadAssetSourceFromContentJSON from './loadAssetSourceFromContentJSON';
-import { caseAssetPath } from '../util/photo-util';
 
 export async function initPhotoEditorUIConfig(
   instance: CreativeEditorSDK,
@@ -348,7 +347,7 @@ async function setupPhotoEditingScene(
   }
   const { width, height } = size;
   // hide page title:
-  engine.editor.setSettingBool('page/title/show', false);
+  engine.editor.setSetting('page/title/show', false);
 
   const scene = engine.scene.create('Free');
   engine.scene.setDesignUnit('Pixel');
@@ -372,10 +371,7 @@ async function setupPhotoEditingScene(
   engine.block.setScopeEnabled(page, 'fill/changeType', false);
   // Disable stroke of page, since it does not make sense with current wording and takes up to much space
   engine.block.setScopeEnabled(page, 'stroke/change', false);
-  engine.editor.setSettingBool(
-    'ubq://page/moveChildrenWhenCroppingFill' as SettingsBool,
-    true
-  );
+  engine.editor.setSetting('page/moveChildrenWhenCroppingFill', true);
   engine.block.setClipped(page, true);
 
   // If nothing is selected: select page by listening to selection changes
